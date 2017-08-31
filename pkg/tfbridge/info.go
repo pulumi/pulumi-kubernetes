4090317868f87bf7933633a718b8133fecca6d89
Luke Hoban <luke@pulumi.com>
2017-08-30 21:54:48 -0700

    Move GitInfo to tfgen
    We only use the Git info in tfgen, not in tfbridge at runtime.  Pulling it out of the shared ProviderInfo so that we don't try to look for a Git repo at tfbridge runtime.
    
    Part of addressing https://github.com/pulumi/pulumi-aws/issues/15.

--- pkg/tfbridge/info.go
+++ pkg/tfbridge/info.go
@@ -3,14 +3,7 @@
 package tfbridge
 
 import (
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
-
 	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/pelletier/go-toml"
-	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi-fabric/pkg/tokens"
 )
 
@@ -18,17 +11,10 @@ import (
 // metadata.  It primarily contains a pointer to the Terraform schema, but can also contain specific name translations.
 type ProviderInfo struct {
 	P         *schema.Provider         // the TF provider/schema.
-	Git       *GitInfo                 // the info about this provider's Git repo.
+	Name      string                   // the TF provider name (e.g. terraform-provider-XXXX).
 	Config    map[string]*SchemaInfo   // a map of TF name to config schema overrides.
 	Resources map[string]*ResourceInfo // a map of TF name to Lumi name; if a type is missing, standard mangling occurs.
 	Overlay   *OverlayInfo             // optional overlay information for augmented code-generation.
-}
-
-// GitInfo contains Git information about a provider.
-type GitInfo struct {
-	Repo   string // the Git repo for this provider.
-	Tag    string // the Git tag info for this provider.
-	Commit string // the Git commit info for this provider.
 }
 
 // ResourceInfo is a top-level type exported by a provider.  This structure can override the type to generate.  It can
@@ -70,79 +56,4 @@ type DefaultInfo struct {
 type OverlayInfo struct {
 	Files   []string
 	Modules map[string]*OverlayInfo
-}
-
-const (
-	tfGitHub         = "github.com"
-	tfProvidersOrg   = "terraform-providers"
-	tfProviderPrefix = "terraform-provider"
-)
-
-// GetGitInfo fetches the taggish and commitish info for a provider's repo.  It prefers to use a Gopkg.lock file, in
-// case dep is being used to vendor, and falls back to looking at the raw Git repo using a standard GOPATH location
-// otherwise.  If neither is found, an error is returned.
-func GetGitInfo(prov string) (*GitInfo, error) {
-	repo := tfGitHub + "/" + tfProvidersOrg + "/" + tfProviderPrefix + "-" + prov
-
-	// First look for a Gopkg.lock file.
-	pkglock, err := toml.LoadFile("Gopkg.lock")
-	if err == nil {
-		// If no error, attempt to use the file.  Otherwise, keep looking for a Git repo.
-		if projs, isprojs := pkglock.Get("projects").([]*toml.Tree); isprojs {
-			for _, proj := range projs {
-				if name, isname := proj.Get("name").(string); isname && name == repo {
-					var tag string
-					if vers, isvers := proj.Get("version").(string); isvers {
-						tag = vers
-					}
-					var commit string
-					if revs, isrevs := proj.Get("revision").(string); isrevs {
-						commit = revs
-					}
-					if tag != "" || commit != "" {
-						return &GitInfo{
-							Repo:   repo,
-							Tag:    tag,
-							Commit: commit,
-						}, nil
-					}
-				}
-			}
-		}
-	}
-
-	// If that didn't work, try the GOPATH for a Git repo.
-	gopath := os.Getenv("GOPATH")
-	if gopath == "" {
-		return nil, errors.New("GOPATH is not set; canot read provider's Git info")
-	}
-	repodir := filepath.Join(gopath, "src", tfGitHub, tfProvidersOrg, tfProviderPrefix+"-"+prov)
-
-	// Make sure the target is actually a Git repository so we can fail with a pretty error if not.
-	if _, staterr := os.Stat(filepath.Join(repodir, ".git")); staterr != nil {
-		return nil, errors.Errorf("%v is not a Git repo, and no vendored copy was found", repodir)
-	}
-
-	// Now launch the Git commands.
-	descCmd := exec.Command("git", "describe", "--all", "--long")
-	descCmd.Dir = repodir
-	descOut, err := descCmd.Output()
-	if err != nil {
-		return nil, err
-	} else if strings.HasSuffix(string(descOut), "\n") {
-		descOut = descOut[:len(descOut)-1]
-	}
-	showRefCmd := exec.Command("git", "show-ref", "HEAD")
-	showRefCmd.Dir = repodir
-	showRefOut, err := showRefCmd.Output()
-	if err != nil {
-		return nil, err
-	} else if strings.HasSuffix(string(showRefOut), "\n") {
-		showRefOut = showRefOut[:len(showRefOut)-1]
-	}
-	return &GitInfo{
-		Repo:   repo,
-		Tag:    string(descOut),
-		Commit: string(showRefOut),
-	}, nil
 }
---
+++ pkg/tfgen/gitinfo.go
@@ -0,0 +1,95 @@
+// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+
+package tfgen
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	toml "github.com/pelletier/go-toml"
+	"github.com/pkg/errors"
+)
+
+// GitInfo contains Git information about a provider.
+type GitInfo struct {
+	Repo   string // the Git repo for this provider.
+	Tag    string // the Git tag info for this provider.
+	Commit string // the Git commit info for this provider.
+}
+
+const (
+	tfGitHub         = "github.com"
+	tfProvidersOrg   = "terraform-providers"
+	tfProviderPrefix = "terraform-provider"
+)
+
+// getGitInfo fetches the taggish and commitish info for a provider's repo.  It prefers to use a Gopkg.lock file, in
+// case dep is being used to vendor, and falls back to looking at the raw Git repo using a standard GOPATH location
+// otherwise.  If neither is found, an error is returned.
+func getGitInfo(prov string) (*GitInfo, error) {
+	repo := tfGitHub + "/" + tfProvidersOrg + "/" + tfProviderPrefix + "-" + prov
+
+	// First look for a Gopkg.lock file.
+	pkglock, err := toml.LoadFile("Gopkg.lock")
+	if err == nil {
+		// If no error, attempt to use the file.  Otherwise, keep looking for a Git repo.
+		if projs, isprojs := pkglock.Get("projects").([]*toml.Tree); isprojs {
+			for _, proj := range projs {
+				if name, isname := proj.Get("name").(string); isname && name == repo {
+					var tag string
+					if vers, isvers := proj.Get("version").(string); isvers {
+						tag = vers
+					}
+					var commit string
+					if revs, isrevs := proj.Get("revision").(string); isrevs {
+						commit = revs
+					}
+					if tag != "" || commit != "" {
+						return &GitInfo{
+							Repo:   repo,
+							Tag:    tag,
+							Commit: commit,
+						}, nil
+					}
+				}
+			}
+		}
+	}
+
+	// If that didn't work, try the GOPATH for a Git repo.
+	gopath := os.Getenv("GOPATH")
+	if gopath == "" {
+		return nil, errors.New("GOPATH is not set; canot read provider's Git info")
+	}
+	repodir := filepath.Join(gopath, "src", tfGitHub, tfProvidersOrg, tfProviderPrefix+"-"+prov)
+
+	// Make sure the target is actually a Git repository so we can fail with a pretty error if not.
+	if _, staterr := os.Stat(filepath.Join(repodir, ".git")); staterr != nil {
+		return nil, errors.Errorf("%v is not a Git repo, and no vendored copy was found", repodir)
+	}
+
+	// Now launch the Git commands.
+	descCmd := exec.Command("git", "describe", "--all", "--long")
+	descCmd.Dir = repodir
+	descOut, err := descCmd.Output()
+	if err != nil {
+		return nil, err
+	} else if strings.HasSuffix(string(descOut), "\n") {
+		descOut = descOut[:len(descOut)-1]
+	}
+	showRefCmd := exec.Command("git", "show-ref", "HEAD")
+	showRefCmd.Dir = repodir
+	showRefOut, err := showRefCmd.Output()
+	if err != nil {
+		return nil, err
+	} else if strings.HasSuffix(string(showRefOut), "\n") {
+		showRefOut = showRefOut[:len(showRefOut)-1]
+	}
+	return &GitInfo{
+		Repo:   repo,
+		Tag:    string(descOut),
+		Commit: string(showRefOut),
+	}, nil
+}
