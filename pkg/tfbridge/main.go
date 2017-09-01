ab8c8eaa0fae26b7b8cc959e1d36233d7feffe83
Luke Hoban <luke@pulumi.com>
2017-09-01 14:04:20 -0700

    Invoke terraform provider in memory
    We were previously spinning up a seperate process with the terraform provider and speaking to it through an RPC interface.
    
    However, we actually already had the implementation of the interface we need to talk to in memory in the tfbridge process already.
    
    So just provide that instance instead of a stub that talks over an RPC interface.
    
    This fixes #15, and should greatly simplify the Travis and Dockerfile environments for running Pulumi programs, as they no longer need an installed terraform-provider-aws binary.

--- pkg/tfbridge/main.go
+++ pkg/tfbridge/main.go
@@ -3,20 +3,11 @@
 package tfbridge
 
 import (
-	"io/ioutil"
-	"log"
-
 	"github.com/pulumi/pulumi-fabric/pkg/util/cmdutil"
 )
 
 // Main launches the tfbridge plugin for a given package pkg and provider prov.
 func Main(pkg string, prov ProviderInfo) {
-	// Suppress logging, since Terraform plugins will echo to log and we want to intercept it ourselves.
-	// IDEA: there is undoubtedly a better way to do this.  It's too bad we are smashing all logging that happens in
-	//     this process (not that we have any).  We could instead fork the process and keep the current one pristine.
-	log.SetOutput(ioutil.Discard)
-
-	// Now serve it up!
 	if err := Serve(pkg, prov); err != nil {
 		cmdutil.ExitError(err.Error())
 	}
--- pkg/tfbridge/serve.go
+++ pkg/tfbridge/serve.go
@@ -3,46 +3,23 @@
 package tfbridge
 
 import (
-	goplugin "github.com/hashicorp/go-plugin"
 	"github.com/pulumi/pulumi-fabric/pkg/resource/provider"
 	"github.com/pulumi/pulumi-fabric/sdk/go/pkg/lumirpc"
 )
 
-// Serve dynamically loads a Terraform plugin, fires up a Lumi resource provider listening to inbound gRPC traffic,
-// and actively bridges between the two, translating calls on one side into calls on the other.
+// Serve fires up a Lumi resource provider listening to inbound gRPC traffic,
+// and translates calls from Lumi into actions against the provided Terraform Provider.
 func Serve(module string, info ProviderInfo) error {
-	var plug *goplugin.Client
-	defer (func() {
-		// If the plugin was initialized, we want to kill it when the program exits.
-		if plug != nil {
-			plug.Kill()
-		}
-	})()
-
 	// Create a new resource provider server and listen for and serve incoming connections.
 	return provider.Main(func(host *provider.HostClient) (lumirpc.ResourceProviderServer, error) {
-		// Manufacture the path to the provider binary.
-		provBin := "terraform-provider-" + module
-
-		// Load up the Terraform plugin dynamically so we can invoke CRUD methods on it.
-		plug, err := Plugin(host, provBin)
-		if err != nil {
-			return nil, err
-		}
-
-		// Put the plugin into debug-only output mode and enable real output just prior to returning.  This is to avoid
-		// spewing configuration information to the output until we're actually performing CRUD operations.
-		plug.Logger.Disable()
-		defer plug.Logger.Enable()
-
 		// Create a new bridge provider.
-		bridge := NewProvider(host, module, plug.Provider, info)
+		bridge := NewProvider(host, module, info.P, info)
 
 		// Configure the provider with all of the state on the Lumi side.
 		// TODO: the semantics of this aren't quite what we want.  We already have some oddness around configuration
 		//     initialization due to module initialization order.  But it's even weirder now because when we
 		//     take this snapshot from the heap is semi-arbitrary.  We need to revist configuration state lifetime.
-		if err = bridge.Configure(); err != nil {
+		if err := bridge.Configure(); err != nil {
 			// TODO: errors need to come back in a friendlier way.  Perhaps Configure belongs on the RPC interface.
 			return nil, err
 		}
