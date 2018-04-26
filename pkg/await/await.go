65cdf41e1fe028aea26fe7bed7268c81555a45f7
Alex Clemmer <clemmer.alexander@gmail.com>
2018-04-25 17:10:35 -0700

    Implement blocking `Delete` operation
    See the previous commit for a detailed description of how blocking
    operations are to be implemented in our Kubernetes provider.
    Essentially, this commit will introduce the ability to wait for
    resources to gracefully terminate (e.g., scale down the replicas).
    
    Similarly to the last commit, the main entry point to this await logic
    is the function:
    
      err := await.Deletion(k.pool, k.client, gvk, namespace, name)
    
    The essential nature of this call is to poll until we are sure that all
    resources have been gracefully deleted, or there is an error.

--- pkg/await/await.go
+++ pkg/await/await.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/pulumi/pulumi-kubernetes/pkg/client"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -89,4 +90,75 @@ func Creation(
 	}
 
 	return clientForResource.Get(obj.GetName(), metav1.GetOptions{})
+}
+
+// Deletion (as the usage, `await.Deletion`, implies) will block until one of the following is true:
+// (1) the Kubernetes resource is reported to be deleted; (2) the initialization timeout has
+// occurred; or (3) an error has occurred while the resource was being deleted.
+func Deletion(
+	pool dynamic.ClientPool, disco discovery.DiscoveryInterface, gvk schema.GroupVersionKind,
+	namespace, name string,
+) error {
+	// Make delete options based on the version of the client.
+	version, err := client.FetchVersion(disco)
+	if err != nil {
+		return err
+	}
+
+	deleteOpts := metav1.DeleteOptions{}
+	if version.Compare(1, 6) < 0 {
+		// 1.5.x option.
+		boolFalse := false
+		deleteOpts.OrphanDependents = &boolFalse
+	} else {
+		// 1.6.x option. (NOTE: Background delete propagation is broken in k8s v1.6, and maybe later.)
+		fg := metav1.DeletePropagationForeground
+		deleteOpts.PropagationPolicy = &fg
+	}
+
+	// Obtain client for the resource being deleted.
+	clientForResource, err := client.FromGVK(pool, disco, gvk, namespace)
+	if err != nil {
+		return err
+	}
+
+	// Issue deletion request.
+	err = clientForResource.Delete(name, &deleteOpts)
+	if err != nil && !errors.IsNotFound(err) {
+		return fmt.Errorf("Could not find resource '%s/%s' for deletion: %s", namespace, name, err)
+	} else if err != nil {
+		return err
+	}
+
+	// Wait until create resolves as success or error.
+	var waitErr error
+	id := fmt.Sprintf("%s/%s", gvk.Version, gvk.Kind)
+	switch id {
+	case "v1/Namespace":
+		waitErr = untilCoreV1NamespaceDeleted(clientForResource, name)
+	case "v1/Pod":
+		waitErr = untilCoreV1PodDeleted(clientForResource, name)
+	case "v1/ReplicationController":
+		waitErr = untilCoreV1ReplicationControllerDeleted(clientForResource, name)
+
+	// TODO(hausdorff): ServiceAccount
+
+	// Cases where no wait is necessary.
+	case "autoscaling/v1/HorizontalPodAutoscaler":
+	case "storage.k8s.io/v1/StorageClass":
+	case "v1/ConfigMap":
+	case "v1/LimitRange":
+	case "v1/PersistentVolume":
+	case "v1/PersistentVolumeClaim":
+	case "v1/ResourceQuota":
+	case "v1/Secret":
+	case "v1/Service":
+		break
+
+	// TODO(hausdorff): Find some sensible default for unknown kinds.
+	default:
+		return fmt.Errorf("Could not find object of type '%s'", id)
+	}
+
+	return waitErr
 }
