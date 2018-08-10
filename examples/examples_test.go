9dc959319b691050569634932b28942e75511d41
Matt Ellis <matt@pulumi.com>
2018-08-10 15:25:20 -0700

    Enable tests

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -91,91 +91,88 @@ func TestExamples(t *testing.T) {
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 				},
 			}),
-			// TODO[pulumi-kubernetes#117]: Enable this when parallelism is turned on.
-			//
-			// base.With(integration.ProgramTestOptions{
-			// 	Dir: path.Join(cwd, "guestbook"),
-			// 	ExtraRuntimeValidation: func(
-			// 		t *testing.T, stackInfo integration.RuntimeValidationStackInfo,
-			// 	) {
-			// 		assert.NotNil(t, stackInfo.Deployment)
-			// 		assert.Equal(t, 8, len(stackInfo.Deployment.Resources))
 
-			// 		sort.Slice(stackInfo.Deployment.Resources, func(i, j int) bool {
-			// 			ri := stackInfo.Deployment.Resources[i]
-			// 			rj := stackInfo.Deployment.Resources[j]
-			// 			riname, _ := openapi.Pluck(ri.Outputs, "live", "metadata", "name")
-			// 			rinamespace, _ := openapi.Pluck(ri.Outputs, "live", "metadata", "namespace")
-			// 			rjname, _ := openapi.Pluck(rj.Outputs, "live", "metadata", "name")
-			// 			rjnamespace, _ := openapi.Pluck(rj.Outputs, "live", "metadata", "namespace")
-			// 			return fmt.Sprintf("%s/%s/%s", ri.URN.Type(), rinamespace, riname) <
-			// 				fmt.Sprintf("%s/%s/%s", rj.URN.Type(), rjnamespace, rjname)
-			// 		})
+			base.With(integration.ProgramTestOptions{
+				Dir: path.Join(cwd, "guestbook"),
+				ExtraRuntimeValidation: func(
+					t *testing.T, stackInfo integration.RuntimeValidationStackInfo,
+				) {
+					assert.NotNil(t, stackInfo.Deployment)
+					assert.Equal(t, 8, len(stackInfo.Deployment.Resources))
 
-			// 		var name interface{}
-			// 		var status interface{}
+					sort.Slice(stackInfo.Deployment.Resources, func(i, j int) bool {
+						ri := stackInfo.Deployment.Resources[i]
+						rj := stackInfo.Deployment.Resources[j]
+						riname, _ := openapi.Pluck(ri.Outputs, "live", "metadata", "name")
+						rinamespace, _ := openapi.Pluck(ri.Outputs, "live", "metadata", "namespace")
+						rjname, _ := openapi.Pluck(rj.Outputs, "live", "metadata", "name")
+						rjnamespace, _ := openapi.Pluck(rj.Outputs, "live", "metadata", "namespace")
+						return fmt.Sprintf("%s/%s/%s", ri.URN.Type(), rinamespace, riname) <
+							fmt.Sprintf("%s/%s/%s", rj.URN.Type(), rjnamespace, rjname)
+					})
 
-			// 		// Verify frontend deployment.
-			// 		frontendDepl := stackInfo.Deployment.Resources[0]
-			// 		assert.Equal(t, tokens.Type("kubernetes:apps/v1:Deployment"), frontendDepl.URN.Type())
-			// 		name, _ = openapi.Pluck(frontendDepl.Outputs, "live", "metadata", "name")
-			// 		assert.Equal(t, "frontend", name)
-			// 		status, _ = openapi.Pluck(frontendDepl.Outputs, "live", "status", "readyReplicas")
-			// 		assert.Equal(t, float64(3), status)
+					var name interface{}
+					var status interface{}
 
-			// 		// Verify redis-master deployment.
-			// 		redisMasterDepl := stackInfo.Deployment.Resources[1]
-			// 		assert.Equal(t, tokens.Type("kubernetes:apps/v1:Deployment"), redisMasterDepl.URN.Type())
-			// 		name, _ = openapi.Pluck(redisMasterDepl.Outputs, "live", "metadata", "name")
-			// 		assert.Equal(t, "redis-master", name)
-			// 		status, _ = openapi.Pluck(redisMasterDepl.Outputs, "live", "status", "readyReplicas")
-			// 		assert.Equal(t, float64(1), status)
+					// Verify frontend deployment.
+					frontendDepl := stackInfo.Deployment.Resources[0]
+					assert.Equal(t, tokens.Type("kubernetes:apps/v1:Deployment"), frontendDepl.URN.Type())
+					name, _ = openapi.Pluck(frontendDepl.Outputs, "live", "metadata", "name")
+					assert.Equal(t, "frontend", name)
+					status, _ = openapi.Pluck(frontendDepl.Outputs, "live", "status", "readyReplicas")
+					assert.Equal(t, float64(3), status)
 
-			// 		// Verify redis-slave deployment.
-			// 		redisSlaveDepl := stackInfo.Deployment.Resources[2]
-			// 		assert.Equal(t, tokens.Type("kubernetes:apps/v1:Deployment"), redisSlaveDepl.URN.Type())
-			// 		name, _ = openapi.Pluck(redisSlaveDepl.Outputs, "live", "metadata", "name")
-			// 		assert.Equal(t, "redis-slave", name)
-			// 		status, _ = openapi.Pluck(redisSlaveDepl.Outputs, "live", "status", "readyReplicas")
-			// 		assert.Equal(t, float64(1), status)
+					// Verify redis-master deployment.
+					redisMasterDepl := stackInfo.Deployment.Resources[1]
+					assert.Equal(t, tokens.Type("kubernetes:apps/v1:Deployment"), redisMasterDepl.URN.Type())
+					name, _ = openapi.Pluck(redisMasterDepl.Outputs, "live", "metadata", "name")
+					assert.Equal(t, "redis-master", name)
+					status, _ = openapi.Pluck(redisMasterDepl.Outputs, "live", "status", "readyReplicas")
+					assert.Equal(t, float64(1), status)
 
-			// 		// Verify frontend service.
-			// 		frontentService := stackInfo.Deployment.Resources[3]
-			// 		assert.Equal(t, tokens.Type("kubernetes:core/v1:Service"), frontentService.URN.Type())
-			// 		name, _ = openapi.Pluck(frontentService.Outputs, "live", "metadata", "name")
-			// 		assert.Equal(t, "frontend", name)
-			// 		status, _ = openapi.Pluck(frontentService.Outputs, "live", "spec", "clusterIP")
-			// 		assert.True(t, len(status.(string)) > 1)
+					// Verify redis-slave deployment.
+					redisSlaveDepl := stackInfo.Deployment.Resources[2]
+					assert.Equal(t, tokens.Type("kubernetes:apps/v1:Deployment"), redisSlaveDepl.URN.Type())
+					name, _ = openapi.Pluck(redisSlaveDepl.Outputs, "live", "metadata", "name")
+					assert.Equal(t, "redis-slave", name)
+					status, _ = openapi.Pluck(redisSlaveDepl.Outputs, "live", "status", "readyReplicas")
+					assert.Equal(t, float64(1), status)
 
-			// 		// Verify redis-master service.
-			// 		redisMasterService := stackInfo.Deployment.Resources[4]
-			// 		assert.Equal(t, tokens.Type("kubernetes:core/v1:Service"), redisMasterService.URN.Type())
-			// 		name, _ = openapi.Pluck(redisMasterService.Outputs, "live", "metadata", "name")
-			// 		assert.Equal(t, "redis-master", name)
-			// 		status, _ = openapi.Pluck(redisMasterService.Outputs, "live", "spec", "clusterIP")
-			// 		assert.True(t, len(status.(string)) > 1)
+					// Verify frontend service.
+					frontentService := stackInfo.Deployment.Resources[3]
+					assert.Equal(t, tokens.Type("kubernetes:core/v1:Service"), frontentService.URN.Type())
+					name, _ = openapi.Pluck(frontentService.Outputs, "live", "metadata", "name")
+					assert.Equal(t, "frontend", name)
+					status, _ = openapi.Pluck(frontentService.Outputs, "live", "spec", "clusterIP")
+					assert.True(t, len(status.(string)) > 1)
 
-			// 		// Verify redis-slave service.
-			// 		redisSlaveService := stackInfo.Deployment.Resources[5]
-			// 		assert.Equal(t, tokens.Type("kubernetes:core/v1:Service"), redisSlaveService.URN.Type())
-			// 		name, _ = openapi.Pluck(redisSlaveService.Outputs, "live", "metadata", "name")
-			// 		assert.Equal(t, "redis-slave", name)
-			// 		status, _ = openapi.Pluck(redisSlaveService.Outputs, "live", "spec", "clusterIP")
-			// 		assert.True(t, len(status.(string)) > 1)
+					// Verify redis-master service.
+					redisMasterService := stackInfo.Deployment.Resources[4]
+					assert.Equal(t, tokens.Type("kubernetes:core/v1:Service"), redisMasterService.URN.Type())
+					name, _ = openapi.Pluck(redisMasterService.Outputs, "live", "metadata", "name")
+					assert.Equal(t, "redis-master", name)
+					status, _ = openapi.Pluck(redisMasterService.Outputs, "live", "spec", "clusterIP")
+					assert.True(t, len(status.(string)) > 1)
 
-			//		// Verify the provider resource.
-			//		provRes := stackInfo.Deployment.Resources[6]
-			//		assert.True(t, providers.IsProviderType(provRes.URN.Type()))
+					// Verify redis-slave service.
+					redisSlaveService := stackInfo.Deployment.Resources[5]
+					assert.Equal(t, tokens.Type("kubernetes:core/v1:Service"), redisSlaveService.URN.Type())
+					name, _ = openapi.Pluck(redisSlaveService.Outputs, "live", "metadata", "name")
+					assert.Equal(t, "redis-slave", name)
+					status, _ = openapi.Pluck(redisSlaveService.Outputs, "live", "spec", "clusterIP")
+					assert.True(t, len(status.(string)) > 1)
 
-			//		// Verify root resource.
-			//		stackRes := stackInfo.Deployment.Resources[7]
-			//		assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-			// 	},
-			// }),
+					// Verify the provider resource.
+					provRes := stackInfo.Deployment.Resources[6]
+					assert.True(t, providers.IsProviderType(provRes.URN.Type()))
 
-			// TODO[pulumi-kubernetes#117]: Enable this when parallelism is turned on.
-			//
-			// base.With(integration.ProgramTestOptions{Dir: path.Join(cwd, "helm")}),
+					// Verify root resource.
+					stackRes := stackInfo.Deployment.Resources[7]
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+				},
+			}),
+
+			base.With(integration.ProgramTestOptions{Dir: path.Join(cwd, "helm")}),
 
 			// TODO(hausdorff): Enable this when we transition to a version of minikube which correctly
 			// reports version.
