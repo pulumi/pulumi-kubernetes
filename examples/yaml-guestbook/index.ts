fc43d33e40076d7b15f8e31fc661a5cf2d3c6923
Alex Clemmer <clemmer.alexander@gmail.com>
2018-08-15 15:10:23 -0700

    Export IP addresses of Helm and YAML examples

--- examples/yaml-guestbook/index.ts
+++ examples/yaml-guestbook/index.ts
@@ -4,3 +4,7 @@ import * as k8s from "@pulumi/kubernetes";
 
 // Create resources from standard Kubernetes guestbook YAML example.
 const guestbook = new k8s.yaml.ConfigGroup("guestbook", { files: "yaml/*.yaml" });
+
+// Export the (cluster-private) IP address of the Guestbook frontend.
+const frontend = guestbook.getResource("v1/Service", "frontend");
+export const frontendIp = frontend.spec.apply(spec => spec.clusterIP);
