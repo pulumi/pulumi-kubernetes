pulumi/pulumi-kubernetes

commit 2efb314c64218897bb88895bc468e7c231c8ee88

Report creation errors even if resource exists
The current await logic for creation fails to account for the case where
an object already exists. Specifically, when we attempt to create a
resource, but one with the same name exists, we will actually report
that as an initialization error instead of a creation error.

This can lead to baffling errors. One example: because Pulumi believes
that `pulumi up` successfully created (but did not initialize) the
resource, a subsequent call to `pulumi destroy` will cause Pulumi to
delete the resource, even though it is not managed by this app, and may
not be managed by Pulumi at all. Another, more subtle example is
captured in #136.

This commit resolves this issue by changing how we detect initialization
errors in the Kubernetes provider. Currently, we will simply check if
the object exists after `await.Creation` fails, which is not reliable,
because we have no guarantee that the object was created during the call
to `await.Creation`. In this commit, we will simply require
`await.Creation` to correctly report whether the error was an
initialization error or not.

Fixes #136.

pkg/await/error.go
@@ -19,15 +19,15 @@ type InitializationError interface {
 
 // cancellationError represents an operation that failed because the user cancelled it.
 type cancellationError struct {
-	objectName string
-	subErrors  []string
+	object    *unstructured.Unstructured
+	subErrors []string
 }
 
 var _ error = (*cancellationError)(nil)
 var _ AggregatedError = (*cancellationError)(nil)
 
 func (ce *cancellationError) Error() string {
-	return fmt.Sprintf("Resource operation was cancelled for '%s'", ce.objectName)
+	return fmt.Sprintf("Resource operation was cancelled for '%s'", ce.object.GetName())
 }
 
 // SubErrors returns the errors that were present when cancellation occurred.
@@ -37,15 +37,15 @@ func (ce *cancellationError) SubErrors() []string {
 
 // timeoutError represents an operation that failed because it timed out.
 type timeoutError struct {
-	objectName string
-	subErrors  []string
+	object    *unstructured.Unstructured
+	subErrors []string
 }
 
 var _ error = (*timeoutError)(nil)
 var _ AggregatedError = (*timeoutError)(nil)
 
 func (te *timeoutError) Error() string {
-	return fmt.Sprintf("Timeout occurred for '%s'", te.objectName)
+	return fmt.Sprintf("Timeout occurred for '%s'", te.object.GetName())
 }
 
 // SubErrors returns the errors that were present when timeout occurred.

pkg/await/apps_deployment.go
@@ -94,6 +94,7 @@ type deploymentInitAwaiter struct {
 
 	deploymentErrors map[string]string
 
+	deployment  *unstructured.Unstructured
 	replicaSets map[string]*unstructured.Unstructured
 	pods        map[string]*unstructured.Unstructured
 }
@@ -109,6 +110,7 @@ func makeDeploymentInitAwaiter(c updateAwaitConfig) *deploymentInitAwaiter {
 
 		deploymentErrors: map[string]string{},
 
+		deployment:  c.currentOutputs,
 		pods:        map[string]*unstructured.Unstructured{},
 		replicaSets: map[string]*unstructured.Unstructured{},
 	}
@@ -246,8 +248,6 @@ func (dia *deploymentInitAwaiter) read(
 func (dia *deploymentInitAwaiter) await(
 	deploymentWatcher, replicaSetWatcher, podWatcher watch.Interface, timeout, period <-chan time.Time,
 ) error {
-	inputPodName := dia.config.currentInputs.GetName()
-
 	dia.config.logStatus(diag.Info, "[1/2] Waiting for app ReplicaSet be marked available")
 
 	for {
@@ -259,13 +259,13 @@ func (dia *deploymentInitAwaiter) await(
 		select {
 		case <-dia.config.ctx.Done():
 			return &cancellationError{
-				objectName: inputPodName,
-				subErrors:  dia.errorMessages(),
+				object:    dia.deployment,
+				subErrors: dia.errorMessages(),
 			}
 		case <-timeout:
 			return &timeoutError{
-				objectName: inputPodName,
-				subErrors:  dia.errorMessages(),
+				object:    dia.deployment,
+				subErrors: dia.errorMessages(),
 			}
 		case <-period:
 			scheduleErrors, containerErrors := dia.aggregatePodErrors()
@@ -334,6 +334,8 @@ func (dia *deploymentInitAwaiter) processDeploymentEvent(event watch.Event) {
 	if event.Type == watch.Deleted {
 		return
 	}
+
+	dia.deployment = deployment
 
 	// Get current generation of the Deployment.
 	dia.currentGeneration = deployment.GetAnnotations()[revision]

pkg/await/apps_deployment_test.go
@@ -140,9 +140,11 @@ func Test_Apps_Deployment(t *testing.T) {
 				// Timeout. Failure.
 				timeout <- time.Now()
 			},
-			expectedError: &timeoutError{objectName: deploymentInputName, subErrors: []string{
-				"Minimum number of live Pods was not attained",
-			}},
+			expectedError: &timeoutError{
+				object: deploymentAdded(inputNamespace, deploymentInputName, revision1),
+				subErrors: []string{
+					"Minimum number of live Pods was not attained",
+				}},
 		},
 		{
 			description: "[Revision 2] Should fail if unrelated Deployment succeeds",
@@ -159,9 +161,11 @@ func Test_Apps_Deployment(t *testing.T) {
 				// Timeout. Failure.
 				timeout <- time.Now()
 			},
-			expectedError: &timeoutError{objectName: deploymentInputName, subErrors: []string{
-				"Minimum number of live Pods was not attained",
-			}},
+			expectedError: &timeoutError{
+				object: deploymentAdded(inputNamespace, deploymentInputName, revision2),
+				subErrors: []string{
+					"Minimum number of live Pods was not attained",
+				}},
 		},
 		{
 			description: "[Revision 1] Should succeed when unrelated deployment fails",
@@ -237,7 +241,8 @@ func Test_Apps_Deployment(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: deploymentInputName, subErrors: []string{
+				object: deploymentAdded(inputNamespace, deploymentInputName, revision1),
+				subErrors: []string{
 					"Minimum number of live Pods was not attained",
 				}},
 		},
@@ -255,7 +260,8 @@ func Test_Apps_Deployment(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: deploymentInputName, subErrors: []string{
+				object: deploymentAdded(inputNamespace, deploymentInputName, revision2),
+				subErrors: []string{
 					"Minimum number of live Pods was not attained",
 				}},
 		},
@@ -273,7 +279,8 @@ func Test_Apps_Deployment(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: deploymentInputName, subErrors: []string{
+				object: deploymentProgressing(inputNamespace, deploymentInputName, revision2),
+				subErrors: []string{
 					"Minimum number of live Pods was not attained"}},
 		},
 		{
@@ -292,7 +299,7 @@ func Test_Apps_Deployment(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: deploymentInputName,
+				object: deploymentProgressingUnavailable(inputNamespace, deploymentInputName, revision2),
 				subErrors: []string{
 					"[MinimumReplicasUnavailable] Deployment does not have minimum availability.",
 					"Minimum number of live Pods was not attained"}},
@@ -309,7 +316,8 @@ func Test_Apps_Deployment(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: deploymentInputName, subErrors: []string{
+				object: deploymentUpdated(inputNamespace, deploymentInputName, revision2),
+				subErrors: []string{
 					"Attempted to roll forward to new ReplicaSet, but minimum number of Pods did not become live"}},
 		},
 		{
@@ -326,7 +334,8 @@ func Test_Apps_Deployment(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: deploymentInputName, subErrors: []string{
+				object: deploymentUpdatedReplicaSetProgressing(inputNamespace, deploymentInputName, revision2),
+				subErrors: []string{
 					"Minimum number of Pods to consider the application live was not attained"}},
 		},
 		{
@@ -363,7 +372,7 @@ func Test_Apps_Deployment(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: deploymentInputName,
+				object: deploymentRevision2Created(inputNamespace, deploymentInputName),
 				subErrors: []string{
 					"Minimum number of Pods to consider the application live was not attained"}},
 		},
@@ -385,7 +394,7 @@ func Test_Apps_Deployment(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: deploymentInputName,
+				object: deploymentNotProgressing(inputNamespace, deploymentInputName, revision2),
 				subErrors: []string{
 					`[ProgressDeadlineExceeded] ReplicaSet "foo-13y9rdnu-b94df86d6" has timed ` +
 						`out progressing.`,
@@ -409,7 +418,8 @@ func Test_Apps_Deployment(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: deploymentInputName, subErrors: []string{
+				object: deploymentProgressingInvalidContainer(inputNamespace, deploymentInputName, revision2),
+				subErrors: []string{
 					"Minimum number of Pods to consider the application live was not attained",
 				}},
 		},
@@ -432,9 +442,11 @@ func Test_Apps_Deployment(t *testing.T) {
 				// Timeout. Failure.
 				timeout <- time.Now()
 			},
-			expectedError: &timeoutError{objectName: deploymentInputName, subErrors: []string{
-				"Minimum number of live Pods was not attained",
-			}},
+			expectedError: &timeoutError{
+				object: deploymentProgressing(inputNamespace, deploymentInputName, revision1),
+				subErrors: []string{
+					"Minimum number of live Pods was not attained",
+				}},
 		},
 		{
 			description: "[Revision 2] Failure should only report Pods from active ReplicaSet, part 1",
@@ -455,9 +467,11 @@ func Test_Apps_Deployment(t *testing.T) {
 				// Timeout. Failure.
 				timeout <- time.Now()
 			},
-			expectedError: &timeoutError{objectName: deploymentInputName, subErrors: []string{
-				"Minimum number of live Pods was not attained",
-			}},
+			expectedError: &timeoutError{
+				object: deploymentProgressing(inputNamespace, deploymentInputName, revision2),
+				subErrors: []string{
+					"Minimum number of live Pods was not attained",
+				}},
 		},
 		{
 			description: "[Revision 1] Failure should only report Pods from active ReplicaSet, part 2",
@@ -478,9 +492,11 @@ func Test_Apps_Deployment(t *testing.T) {
 				// Timeout. Failure.
 				timeout <- time.Now()
 			},
-			expectedError: &timeoutError{objectName: deploymentInputName, subErrors: []string{
-				"Minimum number of live Pods was not attained",
-			}},
+			expectedError: &timeoutError{
+				object: deploymentProgressing(inputNamespace, deploymentInputName, revision1),
+				subErrors: []string{
+					"Minimum number of live Pods was not attained",
+				}},
 		},
 		{
 			description: "[Revision 2] Failure should only report Pods from active ReplicaSet, part 2",
@@ -501,9 +517,11 @@ func Test_Apps_Deployment(t *testing.T) {
 				// Timeout. Failure.
 				timeout <- time.Now()
 			},
-			expectedError: &timeoutError{objectName: deploymentInputName, subErrors: []string{
-				"Minimum number of live Pods was not attained",
-			}},
+			expectedError: &timeoutError{
+				object: deploymentProgressing(inputNamespace, deploymentInputName, revision2),
+				subErrors: []string{
+					"Minimum number of live Pods was not attained",
+				}},
 		},
 		{
 			description: "Should fail if ReplicaSet generations do not match",
@@ -516,8 +534,10 @@ func Test_Apps_Deployment(t *testing.T) {
 				// Timeout. Failure.
 				timeout <- time.Now()
 			},
-			expectedError: &timeoutError{objectName: deploymentInputName, subErrors: []string{
-				"Minimum number of Pods to consider the application live was not attained"}},
+			expectedError: &timeoutError{
+				object: deploymentRolloutComplete(inputNamespace, deploymentInputName, revision1),
+				subErrors: []string{
+					"Minimum number of Pods to consider the application live was not attained"}},
 		},
 	}
 

pkg/await/await.go
@@ -51,15 +51,15 @@ import (
 // occurred; or (3) an error has occurred while the resource was being initialized.
 func Creation(
 	ctx context.Context, host *provider.HostClient, pool dynamic.ClientPool,
-	disco discovery.ServerResourcesInterface, urn resource.URN, obj *unstructured.Unstructured,
+	disco discovery.ServerResourcesInterface, urn resource.URN, inputs *unstructured.Unstructured,
 ) (*unstructured.Unstructured, error) {
-	clientForResource, err := client.FromResource(pool, disco, obj)
+	clientForResource, err := client.FromResource(pool, disco, inputs)
 	if err != nil {
 		return nil, err
 	}
 
 	// Issue create request.
-	_, err = clientForResource.Create(obj)
+	outputs, err := clientForResource.Create(inputs)
 	if err != nil {
 		return nil, err
 	}
@@ -67,7 +67,7 @@ func Creation(
 	// Wait until create resolves as success or error. Note that the conditional is set up to log
 	// only if we don't have an entry for the resource type; in the event that we do, but the await
 	// logic is blank, simply do nothing instead of logging.
-	id := fmt.Sprintf("%s/%s", obj.GetAPIVersion(), obj.GetKind())
+	id := fmt.Sprintf("%s/%s", inputs.GetAPIVersion(), inputs.GetKind())
 	if awaiter, exists := awaiters[id]; exists {
 		if awaiter.awaitCreation != nil {
 			conf := createAwaitConfig{
@@ -77,7 +77,8 @@ func Creation(
 				disco:             disco,
 				clientForResource: clientForResource,
 				urn:               urn,
-				currentInputs:     obj,
+				currentInputs:     inputs,
+				currentOutputs:    outputs,
 			}
 			waitErr := awaiter.awaitCreation(conf)
 			if waitErr != nil {
@@ -89,7 +90,7 @@ func Creation(
 			"No initialization logic found for object of type '%s'; defaulting to assuming initialization successful", id)
 	}
 
-	return clientForResource.Get(obj.GetName(), metav1.GetOptions{})
+	return clientForResource.Get(inputs.GetName(), metav1.GetOptions{})
 }
 
 // Read checks a resource, returning the object if it was created and initialized successfully.
@@ -104,10 +105,13 @@ func Read(
 		return nil, err
 	}
 
-	// No inputs means that we do not manage the resource, i.e., it's a call to
-	// `CustomResource#get`. Simply return the object.
-	if inputs == nil || len(inputs.Object) == 0 {
-		return clientForResource.Get(name, metav1.GetOptions{})
+	outputs, err := clientForResource.Get(name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	} else if inputs == nil || len(inputs.Object) == 0 {
+		// No inputs means that we do not manage the resource, i.e., it's a call to
+		// `CustomResource#get`. Simply return the object.
+		return outputs, nil
 	}
 
 	id := fmt.Sprintf("%s/%s", gvk.GroupVersion(), gvk.Kind)
@@ -121,6 +125,7 @@ func Read(
 				clientForResource: clientForResource,
 				urn:               urn,
 				currentInputs:     inputs,
+				currentOutputs:    outputs,
 			}
 			waitErr := awaiter.awaitRead(conf)
 			if waitErr != nil {
@@ -218,7 +223,7 @@ func Update(
 
 	// Issue patch request. NOTE: We can use the same client because if the `kind` changes, this
 	// will cause a replace (i.e., destroy and create).
-	_, err = clientForResource.Patch(currentSubmitted.GetName(), patchType, patch)
+	currentOutputs, err := clientForResource.Patch(currentSubmitted.GetName(), patchType, patch)
 	if err != nil {
 		return nil, err
 	}
@@ -238,6 +243,7 @@ func Update(
 					clientForResource: clientForResource,
 					urn:               urn,
 					currentInputs:     currentSubmitted,
+					currentOutputs:    currentOutputs,
 				},
 				lastInputs:  lastSubmitted,
 				lastOutputs: liveOldObj,

pkg/await/await_test.go
@@ -1,0 +1,78 @@
+package await
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/pulumi/pulumi-kubernetes/pkg/watcher"
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/dynamic"
+)
+
+func Test_Watcher_Interface_Cancel(t *testing.T) {
+	cancelCtx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	// Cancel should occur before `WatchUntil` because predicate always returns false.
+	err := watcher.ForObject(cancelCtx, &mockResourceInterface{}, "").
+		WatchUntil(func(_ *unstructured.Unstructured) bool { return false }, 1*time.Minute)
+
+	_, isInitErr := err.(InitializationError)
+	assert.True(t, isInitErr, "Cancelled watcher should emit `await.InitializationError`")
+	assert.Equal(t, "Resource operation was cancelled for ''", err.Error())
+}
+
+func Test_Watcher_Interface_Timeout(t *testing.T) {
+	// Timeout because the `WatchUntil` predicate always returns false.
+	err := watcher.ForObject(context.Background(), &mockResourceInterface{}, "").
+		WatchUntil(func(_ *unstructured.Unstructured) bool { return false }, 1*time.Second)
+
+	_, isInitErr := err.(InitializationError)
+	assert.True(t, isInitErr, "Timed out watcher should emit `await.InitializationError`")
+	assert.Equal(t, "Timeout occurred polling for ''", err.Error())
+}
+
+// --------------------------------------------------------------------------
+
+// Mock implementations of Kubernetes client stuff.
+
+// --------------------------------------------------------------------------
+
+type mockResourceInterface struct{}
+
+var _ dynamic.ResourceInterface = (*mockResourceInterface)(nil)
+
+func (mri *mockResourceInterface) List(opts metav1.ListOptions) (runtime.Object, error) {
+	panic("List not implemented")
+}
+func (mri *mockResourceInterface) Get(
+	name string, opts metav1.GetOptions) (*unstructured.Unstructured, error) {
+	return &unstructured.Unstructured{Object: map[string]interface{}{}}, nil
+}
+func (mri *mockResourceInterface) Delete(name string, opts *metav1.DeleteOptions) error {
+	panic("Delete not implemented")
+}
+func (mri *mockResourceInterface) DeleteCollection(
+	deleteOptions *metav1.DeleteOptions, listOptions metav1.ListOptions) error {
+	panic("DeleteCollection not implemented")
+}
+func (mri *mockResourceInterface) Create(
+	obj *unstructured.Unstructured) (*unstructured.Unstructured, error) {
+	panic("Create not implemented")
+}
+func (mri *mockResourceInterface) Update(obj *unstructured.Unstructured) (*unstructured.Unstructured, error) {
+	panic("Update not implemented")
+}
+func (mri *mockResourceInterface) Watch(opts metav1.ListOptions) (watch.Interface, error) {
+	panic("Watch not implemented")
+}
+func (mri *mockResourceInterface) Patch(
+	name string, pt types.PatchType, data []byte) (*unstructured.Unstructured, error) {
+	panic("Patch not implemented")
+}

pkg/await/awaiters.go
@@ -50,6 +50,7 @@ type createAwaitConfig struct {
 	clientForResource dynamic.ResourceInterface
 	urn               resource.URN
 	currentInputs     *unstructured.Unstructured
+	currentOutputs    *unstructured.Unstructured
 }
 
 func (cac *createAwaitConfig) eventClient() (dynamic.ResourceInterface, error) {
@@ -401,7 +402,8 @@ func untilCoreV1ReplicationControllerInitialized(c createAwaitConfig) error {
 	// but that means checking each pod status separately (which can be expensive at scale)
 	// as there's no aggregate data available from the API
 
-	glog.V(3).Infof("Replication controller '%s' initialized: %#v", c.currentInputs)
+	glog.V(3).Infof("Replication controller '%s' initialized: %#v", c.currentInputs.GetName(),
+		c.currentInputs)
 
 	return nil
 }
@@ -465,7 +467,8 @@ func untilCoreV1ResourceQuotaInitialized(c createAwaitConfig) error {
 		hard, hardIsMap := hardRaw.(map[string]interface{})
 		hardStatus, hardStatusIsMap := hardStatusRaw.(map[string]interface{})
 		if hardIsMap && hardStatusIsMap && reflect.DeepEqual(hard, hardStatus) {
-			glog.V(3).Infof("ResourceQuota '%s' initialized: %#v", c.currentInputs.GetName())
+			glog.V(3).Infof("ResourceQuota '%s' initialized: %#v", c.currentInputs.GetName(),
+				c.currentInputs)
 			return true
 		}
 		glog.V(3).Infof("Quotas don't match after creation.\nExpected: %#v\nGiven: %#v",

pkg/await/core_pod.go
@@ -328,12 +328,14 @@ func errorFromCondition(errors map[string]string, condition map[string]interface
 
 type podInitAwaiter struct {
 	podChecker
+	pod    *unstructured.Unstructured
 	config createAwaitConfig
 }
 
 func makePodInitAwaiter(c createAwaitConfig) *podInitAwaiter {
 	return &podInitAwaiter{
 		config:     c,
+		pod:        c.currentOutputs,
 		podChecker: *makePodChecker(),
 	}
 }
@@ -382,8 +384,6 @@ func (pia *podInitAwaiter) read(pod *unstructured.Unstructured) error {
 
 // await is a helper companion to `Await` designed to make it easy to test this module.
 func (pia *podInitAwaiter) await(podWatcher watch.Interface, timeout <-chan time.Time) error {
-	inputPodName := pia.config.currentInputs.GetName()
-
 	pia.config.logStatus(diag.Info, "[1/2] Pulling container images")
 
 	for {
@@ -399,13 +399,13 @@ func (pia *podInitAwaiter) await(podWatcher watch.Interface, timeout <-chan time
 		// ~30 seconds, report that.
 		case <-pia.config.ctx.Done():
 			return &cancellationError{
-				objectName: inputPodName,
-				subErrors:  pia.errorMessages(),
+				object:    pia.pod,
+				subErrors: pia.errorMessages(),
 			}
 		case <-timeout:
 			return &timeoutError{
-				objectName: inputPodName,
-				subErrors:  pia.errorMessages(),
+				object:    pia.pod,
+				subErrors: pia.errorMessages(),
 			}
 		case event := <-podWatcher.ResultChan():
 			pia.processPodEvent(event)
@@ -433,6 +433,8 @@ func (pia *podInitAwaiter) processPodEvent(event watch.Event) {
 	if event.Type == watch.Deleted {
 		return
 	}
+
+	pia.pod = pod
 
 	pia.check(pod)
 }

pkg/await/core_pod_test.go
@@ -52,7 +52,7 @@ func Test_Core_Pod(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: "foo-4setj4y6", subErrors: []string{}},
+				object: podAdded("default", "foo-4setj4y6"), subErrors: []string{}},
 		},
 		{
 			description: "Should fail if Pod is scheduled but containers aren't created",
@@ -64,7 +64,7 @@ func Test_Core_Pod(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: "foo-4setj4y6", subErrors: []string{}},
+				object: podScheduled("default", "foo-4setj4y6"), subErrors: []string{}},
 		},
 		{
 			description: "Should fail if Pod is unschedulable",
@@ -76,7 +76,7 @@ func Test_Core_Pod(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: "foo-4setj4y6",
+				object: podUnschedulable("default", "foo-4setj4y6"),
 				subErrors: []string{
 					"Pod unscheduled: [Unschedulable] No nodes are available that match all " +
 						"of the predicates: Insufficient cpu (3).",
@@ -93,7 +93,7 @@ func Test_Core_Pod(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: "foo-4setj4y6", subErrors: []string{
+				object: podContainerCreating("default", "foo-4setj4y6"), subErrors: []string{
 					"Pod not ready: [ContainersNotReady] containers with unready status: [nginx]",
 				}},
 		},
@@ -107,7 +107,7 @@ func Test_Core_Pod(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: "foo-4setj4y6",
+				object: podErrImagePull("default", "foo-4setj4y6"),
 				subErrors: []string{
 					"Pod not ready: [ContainersNotReady] containers with unready status: [nginx]",
 					"[ErrImagePull] repository dsjkdsjkljks not found: does not exist or no " +
@@ -124,7 +124,7 @@ func Test_Core_Pod(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: "foo-4setj4y6",
+				object: podTerminatedError("default", "foo-4setj4y6"),
 				subErrors: []string{
 					"Pod not ready: [ContainersNotReady] containers with unready status: [completer]",
 					"[RunContainerError] failed to start container " +
@@ -144,7 +144,7 @@ func Test_Core_Pod(t *testing.T) {
 				timeout <- time.Now()
 			},
 			expectedError: &timeoutError{
-				objectName: "foo-4setj4y6",
+				object: podTerminatedSuccess("default", "foo-4setj4y6"),
 				subErrors: []string{
 					"Pod not ready: [ContainersNotReady] containers with unready status: [completer]",
 					"[Completed] Container completed with exit code 0",

commit 558f7e3fe2f7164f70184f5bbda20a2c09f79412

`cancellationError` should implement `InitializationError`
`InitializationError` is used to signal to the provider that a resource
has successfully been created, but not initialized. Currently
`cancellationError` does not implement this interface, so if
`Deployment` or `Service` are cancelled, this is not registered as a
partial failure with the engine or service.

This did not used to be the case, but was a regression with #236.

pkg/await/error.go
@@ -25,6 +25,7 @@ type cancellationError struct {
 
 var _ error = (*cancellationError)(nil)
 var _ AggregatedError = (*cancellationError)(nil)
+var _ InitializationError = (*cancellationError)(nil)
 
 func (ce *cancellationError) Error() string {
 	return fmt.Sprintf("Resource operation was cancelled for '%s'", ce.object.GetName())
@@ -33,6 +34,10 @@ func (ce *cancellationError) Error() string {
 // SubErrors returns the errors that were present when cancellation occurred.
 func (ce *cancellationError) SubErrors() []string {
 	return ce.subErrors
+}
+
+func (ce *cancellationError) Object() *unstructured.Unstructured {
+	return ce.object
 }
 
 // timeoutError represents an operation that failed because it timed out.