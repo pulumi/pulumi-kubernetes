331d81af9ad494405f886d259963addcc1c89c10
Levi Blackstone <levi@pulumi.com>
2018-11-14 14:56:16 -0700

    Add examples for Pluck

---
+++ pkg/openapi/example_openapi_test.go
@@ -0,0 +1,43 @@
+package openapi
+
+import (
+	"fmt"
+)
+
+func ExamplePluck_path_found() {
+	obj := map[string]interface{}{
+		"a": map[string]interface{}{
+			"x": map[string]interface{}{
+				"foo": 1,
+				"bar": 2,
+			},
+		},
+	}
+
+	raw, ok := Pluck(obj, "a", "x", "bar")
+	fmt.Printf("found = %v\n", ok)
+	fmt.Printf("a.x.bar = %v\n", raw)
+
+	// Output:
+	// found = true
+	// a.x.bar = 2
+}
+
+func ExamplePluck_path_not_found() {
+	obj := map[string]interface{}{
+		"a": map[string]interface{}{
+			"x": map[string]interface{}{
+				"foo": 1,
+				"bar": 2,
+			},
+		},
+	}
+
+	raw, ok := Pluck(obj, "a", "x", "baz")
+	fmt.Printf("found = %v\n", ok)
+	fmt.Printf("a.x.baz = %v\n", raw)
+
+	// Output:
+	// found = false
+	// a.x.baz = <nil>
+}
