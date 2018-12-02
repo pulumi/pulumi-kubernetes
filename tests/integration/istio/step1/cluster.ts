9b65118e832f4725bb65f38724aeff63b5ce0cbc
Alex Clemmer <clemmer.alexander@gmail.com>
2018-12-01 21:09:35 -0800

    Add Istio integration test

---
+++ tests/integration/istio/step1/cluster.ts
@@ -0,0 +1,78 @@
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+import * as gcp from "@pulumi/gcp";
+import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
+import * as config from "./config";
+
+const network = new gcp.compute.Network(config.appName, {
+    autoCreateSubnetworks: false,
+    project: config.gcpProject
+});
+
+const subnet = new gcp.compute.Subnetwork(config.appName, {
+    ipCidrRange: "10.0.0.0/24",
+    network: network.name,
+    secondaryIpRanges: [{ rangeName: "pods", ipCidrRange: "10.1.0.0/16" }],
+    project: config.gcpProject,
+    region: config.gcpRegion
+});
+
+// Create the GKE cluster and export it.
+export const k8sCluster = new gcp.container.Cluster(config.appName, {
+    initialNodeCount: config.nodeCount,
+    nodeVersion: "latest",
+    minMasterVersion: "latest",
+    masterAuth: { username: config.masterUsername, password: config.masterPassword },
+    network: network.name,
+    subnetwork: subnet.name,
+    nodeConfig: {
+        machineType: config.nodeMachineType,
+        oauthScopes: [
+            "https://www.googleapis.com/auth/compute",
+            "https://www.googleapis.com/auth/devstorage.read_only",
+            "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/monitoring"
+        ]
+    },
+    project: config.gcpProject,
+    zone: `${config.gcpRegion}-${config.gcpZone}`
+});
+
+// Manufacture a GKE-style Kubeconfig. Note that this is slightly "different" because of the way GKE requires
+// gcloud to be in the picture for cluster authentication (rather than using the client cert/key directly).
+export const k8sConfig = pulumi
+    .all([k8sCluster.name, k8sCluster.endpoint, k8sCluster.masterAuth])
+    .apply(([name, endpoint, auth]) => {
+        const context = `${gcp.config.project}_${gcp.config.zone}_${name}`;
+        return `apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: ${auth.clusterCaCertificate}
+    server: https://${endpoint}
+  name: ${context}
+contexts:
+- context:
+    cluster: ${context}
+    user: ${context}
+  name: ${context}
+current-context: ${context}
+kind: Config
+preferences: {}
+users:
+- name: ${context}
+  user:
+    auth-provider:
+      config:
+        cmd-args: config config-helper --format=json
+        cmd-path: gcloud
+        expiry-key: '{.credential.token_expiry}'
+        token-key: '{.credential.access_token}'
+      name: gcp
+`;
+    });
+
+// Export a Kubernetes provider instance that uses our cluster from above.
+export const k8sProvider = new k8s.Provider(config.appName, {
+    kubeconfig: k8sConfig
+});
---
+++ tests/integration/istio/step1/istio.ts
@@ -0,0 +1,42 @@
+import { execSync } from "child_process";
+
+import * as k8s from "@pulumi/kubernetes";
+
+import * as config from "./config";
+import { k8sProvider } from "./cluster";
+
+const appName = "istio";
+const namespace = new k8s.core.v1.Namespace(
+    `${appName}-system`,
+    { metadata: { name: `${appName}-system` } },
+    { provider: k8sProvider }
+);
+
+const adminBinding = new k8s.rbac.v1.ClusterRoleBinding(
+    "cluster-admin-binding",
+    {
+        metadata: { name: "cluster-admin-binding" },
+        roleRef: {
+            apiGroup: "rbac.authorization.k8s.io",
+            kind: "ClusterRole",
+            name: "cluster-admin"
+        },
+        subjects: [
+            { apiGroup: "rbac.authorization.k8s.io", kind: "User", name: config.gcpUsername }
+        ]
+    },
+    { provider: k8sProvider }
+);
+
+export const istio = new k8s.helm.v2.Chart(
+    appName,
+    {
+        chart: "istio",
+        namespace: namespace.metadata.apply(m => m.name),
+        version: "1.0.1",
+        fetchOpts: { repo: "https://istio.io/charts/" },
+        // for all options check https://github.com/istio/istio/tree/master/install/kubernetes/helm/istio
+        values: { kiali: { enabled: true } }
+    },
+    { dependsOn: [namespace, adminBinding], providers: { kubernetes: k8sProvider } }
+);
