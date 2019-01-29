ad92957eb7f55afe370904ed107a280d8ec0b873
Levi Blackstone <levi@pulumi.com>
2019-01-29 16:13:59 -0700

    Use random namespace for retry test to allow parallelism (#385)

--- tests/integration/retry/retry_test.go
+++ tests/integration/retry/retry_test.go
@@ -1,10 +1,24 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2019, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package ints
 
 import (
 	"os"
 	"testing"
+
+	"github.com/pulumi/pulumi/pkg/tokens"
 
 	"github.com/pulumi/pulumi-kubernetes/pkg/openapi"
 	"github.com/pulumi/pulumi-kubernetes/tests"
@@ -27,23 +41,27 @@ func TestRetry(t *testing.T) {
 		Quick:        true,
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			assert.NotNil(t, stackInfo.Deployment)
-			assert.Equal(t, 4, len(stackInfo.Deployment.Resources))
+			assert.Equal(t, 6, len(stackInfo.Deployment.Resources))
 
 			tests.SortResourcesByURN(stackInfo)
 
-			stackRes := stackInfo.Deployment.Resources[3]
+			stackRes := stackInfo.Deployment.Resources[4]
 			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 
 			provRes := stackInfo.Deployment.Resources[2]
 			assert.True(t, providers.IsProviderType(provRes.URN.Type()))
+
+			// Assert the Namespace was created
+			namespace := stackInfo.Deployment.Resources[0]
+			assert.Equal(t, tokens.Type("kubernetes:core/v1:Namespace"), namespace.URN.Type())
 
 			// Assert the Pod was created
 			pod := stackInfo.Deployment.Resources[1]
 			assert.Equal(t, "nginx", string(pod.URN.Name()))
 			step1Name, _ := openapi.Pluck(pod.Outputs, "metadata", "name")
 			assert.Equal(t, "nginx", step1Name.(string))
-			step1Namespace, _ := openapi.Pluck(pod.Outputs, "metadata", "namespace")
-			assert.Equal(t, "test", step1Namespace.(string))
+			step1PodNamespace, _ := openapi.Pluck(pod.Outputs, "metadata", "namespace")
+			assert.Equal(t, namespace.ID.String(), step1PodNamespace.(string))
 		},
 	})
 }
