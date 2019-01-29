0a7cdb5cc599279285753f9963671d8c865f4ae0
Levi Blackstone <levi@pulumi.com>
2019-01-29 12:41:04 -0700

    Use random namespace for autonaming test to allow parallelism (#378)

--- tests/integration/autonaming/step2/index.ts
+++ tests/integration/autonaming/step2/index.ts
@@ -1,6 +1,20 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2019, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import * as k8s from "@pulumi/kubernetes";
+
+export const namespace = new k8s.core.v1.Namespace("test-namespace");
 
 //
 // The image in the Pod's container has changed, triggering a replace. Because `.metadata.name` is
@@ -8,6 +22,9 @@ import * as k8s from "@pulumi/kubernetes";
 //
 
 const pod = new k8s.core.v1.Pod("autonaming-test", {
+  metadata: {
+    namespace: namespace.metadata.apply(ns => ns.name),
+  },
   spec: {
     containers: [
       {name: "nginx", image: "nginx:1.15-alpine"},
