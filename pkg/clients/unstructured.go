9cdc541712711f9f622de92f6c5f2290a9f1f9b6
Levi Blackstone <levi@pulumi.com>
2019-02-21 15:49:31 -0700

    Add unstructured converter and test fixtures

---
+++ pkg/clients/unstructured.go
@@ -0,0 +1,61 @@
+// Copyright 2016-2019, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clients
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/pulumi/pulumi-kubernetes/pkg/kinds"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/api/extensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func FromUnstructured(obj *unstructured.Unstructured) (metav1.Object, error) {
+	b, err := obj.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+
+	var output metav1.Object
+	switch kinds.Kind(obj.GetKind()) {
+	case kinds.Deployment:
+		output = new(appsv1.Deployment)
+	case kinds.Ingress:
+		output = new(v1beta1.Ingress)
+	case kinds.PersistentVolume:
+		output = new(corev1.PersistentVolume)
+	case kinds.PersistentVolumeClaim:
+		output = new(corev1.PersistentVolumeClaim)
+	case kinds.Pod:
+		output = new(corev1.Pod)
+	case kinds.ReplicaSet:
+		output = new(appsv1.ReplicaSet)
+	case kinds.StatefulSet:
+		output = new(appsv1.StatefulSet)
+	default:
+		return nil, fmt.Errorf("unhandled Kind: %s", obj.GetKind())
+	}
+
+	err = json.Unmarshal(b, &output)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
---
+++ pkg/clients/unstructured_test.go
@@ -0,0 +1,56 @@
+// Copyright 2016-2019, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clients
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/pulumi/pulumi-kubernetes/pkg/await/fixtures"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func TestFromUnstructured(t *testing.T) {
+	pod := fixtures.PodBasic()
+	unsPod := fixtures.PodBasic_Uns()
+	deployment := fixtures.DeploymentBasic()
+	unsDeployment := fixtures.DeploymentBasic_Uns()
+
+	type args struct {
+		obj *unstructured.Unstructured
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    v1.Object
+		wantErr bool
+	}{
+		{"valid-pod", args{obj: unsPod}, v1.Object(pod), false},
+		{"valid-deployment", args{obj: unsDeployment}, v1.Object(deployment), false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FromUnstructured(tt.args.obj)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FromUnstructured() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FromUnstructured() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
