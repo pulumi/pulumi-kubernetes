ace2c170daf73b8696a5f11705343a11485b0333
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-02-25 12:37:22 -0800

    test-all should not rerun test-fast

--- tests/examples/examples_test.go
+++ tests/examples/examples_test.go
@@ -37,120 +37,124 @@ func TestExamples(t *testing.T) {
 		},
 	}
 
-	examples := []integration.ProgramTestOptions{
+	shortTests := []integration.ProgramTestOptions{
 		base.With(integration.ProgramTestOptions{Dir: path.Join(cwd, "minimal")}),
 	}
-	if !testing.Short() {
-		examples = append(examples, []integration.ProgramTestOptions{
-			base.With(integration.ProgramTestOptions{
-				Dir: path.Join(cwd, "guestbook"),
-				ExtraRuntimeValidation: func(
-					t *testing.T, stackInfo integration.RuntimeValidationStackInfo,
-				) {
-					assert.NotNil(t, stackInfo.Deployment)
-					assert.Equal(t, 9, len(stackInfo.Deployment.Resources))
 
-					sort.Slice(stackInfo.Deployment.Resources, func(i, j int) bool {
-						ri := stackInfo.Deployment.Resources[i]
-						rj := stackInfo.Deployment.Resources[j]
-						riname, _ := openapi.Pluck(ri.Outputs, "metadata", "name")
-						rinamespace, _ := openapi.Pluck(ri.Outputs, "metadata", "namespace")
-						rjname, _ := openapi.Pluck(rj.Outputs, "metadata", "name")
-						rjnamespace, _ := openapi.Pluck(rj.Outputs, "metadata", "namespace")
-						return fmt.Sprintf("%s/%s/%s", ri.URN.Type(), rinamespace, riname) <
-							fmt.Sprintf("%s/%s/%s", rj.URN.Type(), rjnamespace, rjname)
-					})
+	longTests := []integration.ProgramTestOptions{
+		base.With(integration.ProgramTestOptions{
+			Dir: path.Join(cwd, "guestbook"),
+			ExtraRuntimeValidation: func(
+				t *testing.T, stackInfo integration.RuntimeValidationStackInfo,
+			) {
+				assert.NotNil(t, stackInfo.Deployment)
+				assert.Equal(t, 9, len(stackInfo.Deployment.Resources))
 
-					var name interface{}
-					var status interface{}
+				sort.Slice(stackInfo.Deployment.Resources, func(i, j int) bool {
+					ri := stackInfo.Deployment.Resources[i]
+					rj := stackInfo.Deployment.Resources[j]
+					riname, _ := openapi.Pluck(ri.Outputs, "metadata", "name")
+					rinamespace, _ := openapi.Pluck(ri.Outputs, "metadata", "namespace")
+					rjname, _ := openapi.Pluck(rj.Outputs, "metadata", "name")
+					rjnamespace, _ := openapi.Pluck(rj.Outputs, "metadata", "namespace")
+					return fmt.Sprintf("%s/%s/%s", ri.URN.Type(), rinamespace, riname) <
+						fmt.Sprintf("%s/%s/%s", rj.URN.Type(), rjnamespace, rjname)
+				})
 
-					// Verify frontend deployment.
-					frontendDepl := stackInfo.Deployment.Resources[0]
-					assert.Equal(t, tokens.Type("kubernetes:apps/v1:Deployment"), frontendDepl.URN.Type())
-					name, _ = openapi.Pluck(frontendDepl.Outputs, "metadata", "name")
-					assert.Equal(t, "frontend", name)
-					status, _ = openapi.Pluck(frontendDepl.Outputs, "status", "readyReplicas")
-					assert.Equal(t, float64(3), status)
+				var name interface{}
+				var status interface{}
 
-					// Verify redis-master deployment.
-					redisMasterDepl := stackInfo.Deployment.Resources[1]
-					assert.Equal(t, tokens.Type("kubernetes:apps/v1:Deployment"), redisMasterDepl.URN.Type())
-					name, _ = openapi.Pluck(redisMasterDepl.Outputs, "metadata", "name")
-					assert.Equal(t, "redis-master", name)
-					status, _ = openapi.Pluck(redisMasterDepl.Outputs, "status", "readyReplicas")
-					assert.Equal(t, float64(1), status)
+				// Verify frontend deployment.
+				frontendDepl := stackInfo.Deployment.Resources[0]
+				assert.Equal(t, tokens.Type("kubernetes:apps/v1:Deployment"), frontendDepl.URN.Type())
+				name, _ = openapi.Pluck(frontendDepl.Outputs, "metadata", "name")
+				assert.Equal(t, "frontend", name)
+				status, _ = openapi.Pluck(frontendDepl.Outputs, "status", "readyReplicas")
+				assert.Equal(t, float64(3), status)
 
-					// Verify redis-slave deployment.
-					redisSlaveDepl := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, tokens.Type("kubernetes:apps/v1:Deployment"), redisSlaveDepl.URN.Type())
-					name, _ = openapi.Pluck(redisSlaveDepl.Outputs, "metadata", "name")
-					assert.Equal(t, "redis-slave", name)
-					status, _ = openapi.Pluck(redisSlaveDepl.Outputs, "status", "readyReplicas")
-					assert.Equal(t, float64(1), status)
+				// Verify redis-master deployment.
+				redisMasterDepl := stackInfo.Deployment.Resources[1]
+				assert.Equal(t, tokens.Type("kubernetes:apps/v1:Deployment"), redisMasterDepl.URN.Type())
+				name, _ = openapi.Pluck(redisMasterDepl.Outputs, "metadata", "name")
+				assert.Equal(t, "redis-master", name)
+				status, _ = openapi.Pluck(redisMasterDepl.Outputs, "status", "readyReplicas")
+				assert.Equal(t, float64(1), status)
 
-					// Verify test namespace.
-					namespace := stackInfo.Deployment.Resources[3]
-					assert.Equal(t, tokens.Type("kubernetes:core/v1:Namespace"), namespace.URN.Type())
+				// Verify redis-slave deployment.
+				redisSlaveDepl := stackInfo.Deployment.Resources[2]
+				assert.Equal(t, tokens.Type("kubernetes:apps/v1:Deployment"), redisSlaveDepl.URN.Type())
+				name, _ = openapi.Pluck(redisSlaveDepl.Outputs, "metadata", "name")
+				assert.Equal(t, "redis-slave", name)
+				status, _ = openapi.Pluck(redisSlaveDepl.Outputs, "status", "readyReplicas")
+				assert.Equal(t, float64(1), status)
 
-					// Verify frontend service.
-					frontentService := stackInfo.Deployment.Resources[4]
-					assert.Equal(t, tokens.Type("kubernetes:core/v1:Service"), frontentService.URN.Type())
-					name, _ = openapi.Pluck(frontentService.Outputs, "metadata", "name")
-					assert.Equal(t, "frontend", name)
-					status, _ = openapi.Pluck(frontentService.Outputs, "spec", "clusterIP")
-					assert.True(t, len(status.(string)) > 1)
+				// Verify test namespace.
+				namespace := stackInfo.Deployment.Resources[3]
+				assert.Equal(t, tokens.Type("kubernetes:core/v1:Namespace"), namespace.URN.Type())
 
-					// Verify redis-master service.
-					redisMasterService := stackInfo.Deployment.Resources[5]
-					assert.Equal(t, tokens.Type("kubernetes:core/v1:Service"), redisMasterService.URN.Type())
-					name, _ = openapi.Pluck(redisMasterService.Outputs, "metadata", "name")
-					assert.Equal(t, "redis-master", name)
-					status, _ = openapi.Pluck(redisMasterService.Outputs, "spec", "clusterIP")
-					assert.True(t, len(status.(string)) > 1)
+				// Verify frontend service.
+				frontentService := stackInfo.Deployment.Resources[4]
+				assert.Equal(t, tokens.Type("kubernetes:core/v1:Service"), frontentService.URN.Type())
+				name, _ = openapi.Pluck(frontentService.Outputs, "metadata", "name")
+				assert.Equal(t, "frontend", name)
+				status, _ = openapi.Pluck(frontentService.Outputs, "spec", "clusterIP")
+				assert.True(t, len(status.(string)) > 1)
 
-					// Verify redis-slave service.
-					redisSlaveService := stackInfo.Deployment.Resources[6]
-					assert.Equal(t, tokens.Type("kubernetes:core/v1:Service"), redisSlaveService.URN.Type())
-					name, _ = openapi.Pluck(redisSlaveService.Outputs, "metadata", "name")
-					assert.Equal(t, "redis-slave", name)
-					status, _ = openapi.Pluck(redisSlaveService.Outputs, "spec", "clusterIP")
-					assert.True(t, len(status.(string)) > 1)
+				// Verify redis-master service.
+				redisMasterService := stackInfo.Deployment.Resources[5]
+				assert.Equal(t, tokens.Type("kubernetes:core/v1:Service"), redisMasterService.URN.Type())
+				name, _ = openapi.Pluck(redisMasterService.Outputs, "metadata", "name")
+				assert.Equal(t, "redis-master", name)
+				status, _ = openapi.Pluck(redisMasterService.Outputs, "spec", "clusterIP")
+				assert.True(t, len(status.(string)) > 1)
 
-					// Verify the provider resource.
-					provRes := stackInfo.Deployment.Resources[7]
-					assert.True(t, providers.IsProviderType(provRes.URN.Type()))
+				// Verify redis-slave service.
+				redisSlaveService := stackInfo.Deployment.Resources[6]
+				assert.Equal(t, tokens.Type("kubernetes:core/v1:Service"), redisSlaveService.URN.Type())
+				name, _ = openapi.Pluck(redisSlaveService.Outputs, "metadata", "name")
+				assert.Equal(t, "redis-slave", name)
+				status, _ = openapi.Pluck(redisSlaveService.Outputs, "spec", "clusterIP")
+				assert.True(t, len(status.(string)) > 1)
 
-					// Verify root resource.
-					stackRes := stackInfo.Deployment.Resources[8]
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-				},
-			}),
+				// Verify the provider resource.
+				provRes := stackInfo.Deployment.Resources[7]
+				assert.True(t, providers.IsProviderType(provRes.URN.Type()))
 
-			base.With(integration.ProgramTestOptions{
-				Dir: path.Join(cwd, "provider"),
-			}),
+				// Verify root resource.
+				stackRes := stackInfo.Deployment.Resources[8]
+				assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+			},
+		}),
 
-			base.With(integration.ProgramTestOptions{
-				SkipRefresh: true, // Deployment controller changes object out-of-band.
-				Dir:         path.Join(cwd, "helm"),
-			}),
+		base.With(integration.ProgramTestOptions{
+			Dir: path.Join(cwd, "provider"),
+		}),
 
-			base.With(integration.ProgramTestOptions{
-				Dir:         path.Join(cwd, "helm-local"),
-				SkipRefresh: true, // Deployment controller changes object out-of-band.
-				ExtraRuntimeValidation: func(
-					t *testing.T, stackInfo integration.RuntimeValidationStackInfo,
-				) {
-					assert.NotNil(t, stackInfo.Deployment)
-					assert.Equal(t, 9, len(stackInfo.Deployment.Resources))
-				},
-			}),
+		base.With(integration.ProgramTestOptions{
+			SkipRefresh: true, // Deployment controller changes object out-of-band.
+			Dir:         path.Join(cwd, "helm"),
+		}),
 
-			base.With(integration.ProgramTestOptions{Dir: path.Join(cwd, "mariadb")}),
-		}...)
+		base.With(integration.ProgramTestOptions{
+			Dir:         path.Join(cwd, "helm-local"),
+			SkipRefresh: true, // Deployment controller changes object out-of-band.
+			ExtraRuntimeValidation: func(
+				t *testing.T, stackInfo integration.RuntimeValidationStackInfo,
+			) {
+				assert.NotNil(t, stackInfo.Deployment)
+				assert.Equal(t, 9, len(stackInfo.Deployment.Resources))
+			},
+		}),
+
+		base.With(integration.ProgramTestOptions{Dir: path.Join(cwd, "mariadb")}),
 	}
 
-	for _, ex := range examples {
+	tests := shortTests
+	if !testing.Short() {
+		tests = append(tests, longTests...)
+	}
+
+	for _, ex := range tests {
 		example := ex
 		t.Run(example.Dir, func(t *testing.T) {
 			integration.ProgramTest(t, &example)
