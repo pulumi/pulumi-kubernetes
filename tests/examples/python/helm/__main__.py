c61ffff6cd04507d6c5dcd2c18c40bdecbfe1b84
Levi Blackstone <levi@pulumi.com>
2019-05-10 15:56:09 -0600

    Allow parallelism on Python Helm test

--- tests/examples/python/helm/__main__.py
+++ tests/examples/python/helm/__main__.py
@@ -11,6 +11,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from pulumi_kubernetes.core.v1 import Namespace
 from pulumi_kubernetes.helm.v2 import Chart, ChartOpts
 
-Chart("nginx-ingress", ChartOpts("stable/nginx-ingress"))
+ns = Namespace("ns")
+
+
+def set_namespace(obj):
+    if "metadata" in obj:
+        obj["metadata"]["namespace"] = ns.metadata["name"]
+    else:
+        obj["metadata"] = {"namespace": ns.metadata["name"]}
+
+    return obj
+
+
+def cluster_ip(obj):
+    if obj["kind"] == "Service" and obj["apiVersion"] == "v1":
+        if "spec" in obj and "type" in obj["spec"] and obj["spec"]["type"] == "LoadBalancer":
+            obj["spec"]["type"] = "ClusterIP"
+
+    return obj
+
+
+Chart("nginx-lego", ChartOpts(
+    "stable/nginx-lego",
+    version="0.3.1",
+    values={"nginx": None, "default": None, "lego": None},
+    transformations=[cluster_ip, set_namespace]))
