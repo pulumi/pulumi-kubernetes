9359ba57c77d381e5d7b19fc17d05bcddc3bc342
Pat Gavlin <pat@pulumi.com>
2019-07-24 13:34:30 -0700

    Update tests/integration/dry-run/step1/index.ts
    Co-Authored-By: Levi Blackstone <levi@pulumi.com>

--- tests/integration/dry-run/step1/index.ts
+++ tests/integration/dry-run/step1/index.ts
@@ -38,4 +38,4 @@ const deployment = new k8s.apps.v1.Deployment("nginx", {
         }
     }
 });
-export const name = deployment.metadata.apply(m => m.name);
+export const name = deployment.metadata.name;
