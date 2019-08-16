720c5cc3470a8d00370c8f892d3eda9fb75a6194
Levi Blackstone <levi@pulumi.com>
2019-08-16 15:24:17 -0600

    Re-enable GOPROXY on 'make ensure'
    Using the proxy speeds up CI builds by several minutes,
    so enable it by default. Add the NOPROXY env var to
    optionally disable the proxy.

--- build/common.mk
+++ build/common.mk
@@ -106,6 +106,7 @@ PULUMI_NODE_MODULES := $(PULUMI_ROOT)/node_modules
 
 GO_TEST_FAST = go test -short -v -count=1 -cover -timeout 2h -parallel ${TESTPARALLELISM}
 GO_TEST = go test -v -count=1 -cover -timeout 2h -parallel ${TESTPARALLELISM}
+GOPROXY = 'https://proxy.golang.org'
 
 .PHONY: default all ensure only_build only_test build lint install test_all core
 
@@ -141,8 +142,13 @@ all:: build install lint test_all
 
 ensure::
 	$(call STEP_MESSAGE)
+ifeq ($(NOPROXY), true)
 	@echo "GO111MODULE=on go mod tidy"; GO111MODULE=on go mod tidy
 	@echo "GO111MODULE=on go mod vendor"; GO111MODULE=on go mod vendor
+else
+	@echo "GO111MODULE=on GOPROXY=$(GOPROXY) go mod tidy"; GO111MODULE=on GOPROXY=$(GOPROXY) go mod tidy
+	@echo "GO111MODULE=on GOPROXY=$(GOPROXY) go mod vendor"; GO111MODULE=on GOPROXY=$(GOPROXY) go mod vendor
+endif
 	@if [ -e 'package.json' ]; then echo "yarn install"; yarn install; fi
 
 build::
