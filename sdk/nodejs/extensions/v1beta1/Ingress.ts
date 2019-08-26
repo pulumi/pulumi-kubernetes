d1931c83d86bd61ba9037c4a2289a627575f22a4
Levi Blackstone <levi@pulumi.com>
2019-08-26 12:38:35 -0600

    Update comment on overriding resource timeouts

--- sdk/nodejs/extensions/v1beta1/Ingress.ts
+++ sdk/nodejs/extensions/v1beta1/Ingress.ts
@@ -25,9 +25,9 @@ import { getVersion } from "../../version";
      * 
      * If the Ingress has not reached a Ready state after 10 minutes, it will
      * time out and mark the resource update as Failed. You can override the default timeout value
-     * by setting 'pulumi.com/timeoutSeconds' as a '.metadata.annotation' on the resource.
-     * This approach will be deprecated in favor of customTimeouts. See
-     * https://github.com/pulumi/pulumi-kubernetes/issues/672 for details.
+     * by setting the 'customTimeouts' option on the resource.
+     * Note that the timeout value for Delete is not yet supported. See
+     * https://github.com/pulumi/pulumi-kubernetes/issues/746 for details.
      */
     export class Ingress extends pulumi.CustomResource {
       /**
