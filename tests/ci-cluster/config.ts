1c03d72effabca532137ebf4a4550e64a97e1b1f
Levi Blackstone <levi@pulumi.com>
2019-08-30 14:32:54 -0600

    Shrink the CI GKE cluster to 3 nodes (#778)

--- tests/ci-cluster/config.ts
+++ tests/ci-cluster/config.ts
@@ -21,8 +21,8 @@ export const gcpProject = "pulumi-development";
 export const gcpZone = "a";
 export const gcpLocation = "us-west1-a";
 
-// nodeCount is the number of cluster nodes to provision. Defaults to 5 if unspecified.
-export const nodeCount = config.getNumber("nodeCount") || 5;
+// nodeCount is the number of cluster nodes to provision. Defaults to 3 if unspecified.
+export const nodeCount = config.getNumber("nodeCount") || 3;
 
 // nodeMachineType is the machine type to use for cluster nodes. Defaults to n1-standard-2 if unspecified.
 // See https://cloud.google.com/compute/docs/machine-types for more details on available machine types.
