6479177f5248149494205c6b51061d538b29f758
Levi Blackstone <levi@pulumi.com>
2019-09-09 12:10:57 -0600

    Change provider test to use ambient kubeconfig (#790)
    The provider test previously tried to load `~/.kube/config` explicitly,
    but this file is not guaranteed to be present. Don't set a kubeconfig
    explicitly on the provider, and it will pick up the ambient config if one
    exists.

--- tests/examples/provider/index.ts
+++ tests/examples/provider/index.ts
@@ -1,21 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as k8s from "@pulumi/kubernetes";
-import * as fs from "fs";
-import * as os from "os";
-import * as path from "path";
-
-// Use the existing ~/.kube/config kubeconfig
-const kubeconfig = fs.readFileSync(path.join(os.homedir(), ".kube", "config")).toString();
 
 // Create a new provider
-const myk8s = new k8s.Provider("myk8s", {
-    kubeconfig: kubeconfig,
-});
+const myk8s = new k8s.Provider("myk8s", {});
 
 // Create a new provider with dry run enabled.
 const myk8s2 = new k8s.Provider("myk8s2", {
-    kubeconfig: kubeconfig,
     enableDryRun: true,
 });
 
