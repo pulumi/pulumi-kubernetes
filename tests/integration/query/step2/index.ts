835c2a6a44366eedcf80d957a3f4cd0eca9a2d89
Levi Blackstone <levi@pulumi.com>
2019-11-04 14:35:43 -0700

    Try removing moolumi prefix since we're using local backend

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -22,7 +22,7 @@ import * as k8s from "@pulumi/kubernetes";
 pulumi.runtime
     .listResourceOutputs(
         k8s.core.v1.Pod.isInstance,
-        `moolumi/query-test-c186bcc3-1572-44d8-b7d5-1028853682c3`,
+        `query-test-c186bcc3-1572-44d8-b7d5-1028853682c3`,
     )
     .toArray()
     .then(pods => {
