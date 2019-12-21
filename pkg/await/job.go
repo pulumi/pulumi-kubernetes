e5177f3103d9166688399dd4dbc93cc55782676e
Levi Blackstone <levi@pulumi.com>
2019-12-20 17:33:31 -0700

    Address feedback

--- pkg/await/job.go
+++ pkg/await/job.go
@@ -180,12 +180,11 @@ func (jia *jobInitAwaiter) Read() error {
 }
 
 func (jia *jobInitAwaiter) processJobEvent(event watch.Event) error {
-	uns, ok := event.Object.(*unstructured.Unstructured)
-	if !ok {
+	if event.Object == nil {
 		glog.V(3).Infof("received event with nil Object: %#v", event)
 		return nil
 	}
-	job, err := clients.JobFromUnstructured(uns)
+	job, err := clients.JobFromUnstructured(event.Object.(*unstructured.Unstructured))
 	if err != nil {
 		glog.V(3).Infof("Failed to unmarshal Job event: %v", err)
 		return nil
