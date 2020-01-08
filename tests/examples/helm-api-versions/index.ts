296b19657e5b1c9bbdb7f6aa7481bab26ededa3b
Richard Laub <richard.laub@mindbodyonline.com>
2020-01-08 14:51:00 -0800

    Update tests to test a single apiVersion

--- tests/examples/helm-api-versions/index.ts
+++ tests/examples/helm-api-versions/index.ts
@@ -1,19 +1,15 @@
 import * as k8s from "@pulumi/kubernetes";
 
 const namespace = new k8s.core.v1.Namespace("test");
-const namespaceName = namespace.metadata.name;
 
 new k8s.helm.v2.Chart("api-versions", {
   apiVersions: ["foo", "bar"],
+  namespace: namespace.metadata.name,
   path: "helm-api-versions",
-  transformations: [
-    // Put every resource in the created namespace.
-    (obj: any) => {
-      if (obj.metadata !== undefined) {
-        obj.metadata.namespace = namespaceName;
-      } else {
-        obj.metadata = { namespace: namespaceName };
-      }
-    }
-  ]
+});
+
+new k8s.helm.v2.Chart("single-api-version", {
+  apiVersions: ["foo"],
+  namespace: namespace.metadata.name,
+  path: "helm-single-api-version",
 });
