97e1c53985c9828b055dfe3eddc8eda1f788ecdf
Levi Blackstone <levi@pulumi.com>
2020-05-14 15:06:21 -0600

    Fix test

--- tests/integration/go/go_test.go
+++ tests/integration/go/go_test.go
@@ -33,9 +33,10 @@ func TestGo_Basic(t *testing.T) {
 func TestGo_YAML(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir: "yaml",
-		Dependencies: []string{
-			"github.com/pulumi/pulumi-kubernetes/sdk/v2",
-		},
+		// TODO: uncomment and delete go.mod/go.sum once apiextensions.CustomResource is available in a tagged release.
+		//Dependencies: []string{
+		//	"github.com/pulumi/pulumi-kubernetes/sdk/v2",
+		//},
 		Quick: true,
 	})
 }
