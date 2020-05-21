e224cd3bbd9aac3ef64044d05108ea0efd8cc2fd
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-05-21 10:15:19 -0700

    Stop publishing tarballs to eng.pulumi.com in publish_tgz.sh.

--- scripts/publish_tgz.sh
+++ scripts/publish_tgz.sh
@@ -13,19 +13,6 @@ if [ ! -f $PUBLISH ]; then
     exit 1
 fi
 
-echo "Publishing SDK build to s3://eng.pulumi.com/:"
-for OS in "${PUBLISH_GOOS[@]}"
-do
-    for ARCH in "${PUBLISH_GOARCH[@]}"
-    do
-        export GOOS=${OS}
-        export GOARCH=${ARCH}
-
-        RELEASE_INFO=($($(dirname $0)/make_release.sh))
-        ${PUBLISH} ${RELEASE_INFO[0]} "${PUBLISH_PROJECT}/${OS}/${ARCH}" ${RELEASE_INFO[@]:1}
-    done
-done
-
 echo "Publishing Plugin archive to s3://rel.pulumi.com/:"
 for OS in "${PUBLISH_GOOS[@]}"
 do
