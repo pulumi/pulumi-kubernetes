8484613d408c24e1e1ca19e0d11baee686d7834e
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-05-21 10:22:24 -0700

    Remove unused vars.

--- scripts/publish_tgz.sh
+++ scripts/publish_tgz.sh
@@ -3,15 +3,8 @@
 set -o nounset -o errexit -o pipefail
 
 ROOT=$(dirname $0)/..
-PUBLISH=$GOPATH/src/github.com/pulumi/scripts/ci/publish.sh
 PUBLISH_GOOS=("linux" "windows" "darwin")
 PUBLISH_GOARCH=("amd64")
-PUBLISH_PROJECT="pulumi-kubernetes"
-
-if [ ! -f $PUBLISH ]; then
-    >&2 echo "error: Missing publish script at $PUBLISH"
-    exit 1
-fi
 
 echo "Publishing Plugin archive to s3://rel.pulumi.com/:"
 for OS in "${PUBLISH_GOOS[@]}"
