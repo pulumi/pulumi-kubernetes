e0617dcf346d3f5aa31a7a57a73dabe036750bcb
Lee Briggs <lee@leebriggs.co.uk>
2020-08-12 13:36:31 -0700

    update python sdk

--- sdk/python/pulumi_kubernetes/helm/v2/helm.py
+++ sdk/python/pulumi_kubernetes/helm/v2/helm.py
@@ -547,7 +547,7 @@ def _parse_chart(all_config: Tuple[str, Union[ChartOpts, LocalChartOpts], pulumi
     pulumi.Output.all(file, data).apply(_write_override_file)
 
     namespace_arg = ['--namespace', config.namespace] if config.namespace else []
-    crd_arg = ['--include-crds'] if _is_helm_v3() and config.install_crds is not False else []
+    crd_arg = ['--include-crds'] if _is_helm_v3() and config.install_crds else []
 
     # Use 'helm template' to create a combined YAML manifest.
     cmd = ['helm', 'template', chart, '--name-template', release_name,
