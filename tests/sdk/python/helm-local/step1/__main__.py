4ed7c7f5288f7a680c01a22f0aa6f21daf3d3b4d
Levi Blackstone <levi@pulumi.com>
2020-08-26 10:23:02 -0600

    Verify no changes from v2 -> v3

---
+++ tests/sdk/python/helm-local/step1/__main__.py
@@ -0,0 +1,22 @@
+# Copyright 2016-2020, Pulumi Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from pulumi_kubernetes.helm.v2 import Chart, LocalChartOpts
+
+values = {"service": {"type": "ClusterIP"}}
+
+Chart("nginx", LocalChartOpts(path="nginx", values=values))
+
+# Deploy a duplicate chart with a different resource prefix to verify that multiple instances of the Chart
+# can be managed in the same stack.
+Chart("nginx", LocalChartOpts(path="nginx", resource_prefix="dup", values=values))
