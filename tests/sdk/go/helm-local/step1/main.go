42e595b6ad3241e22a3e4813d0310b142068dffa
Levi Blackstone <levi@pulumi.com>
2020-09-08 17:57:15 -0600

    Update tests

---
+++ tests/sdk/go/helm-local/step1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/helm/v2"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		chart, err := helm.NewChart(ctx, "test", helm.ChartArgs{
+			Path:           pulumi.String("nginx"),
+			Values:         pulumi.Map{"service": pulumi.StringMap{"type": pulumi.String("ClusterIP")}},
+			ResourcePrefix: "prefix",
+		})
+		if err != nil {
+			return err
+		}
+
+		svc := chart.GetResource("v1/Service", "prefix-prefix-test-nginx", "").
+			Apply(func(r interface{}) (interface{}, error) {
+				svc := r.(*corev1.Service)
+				return svc.Spec.ClusterIP(), nil
+			})
+		ctx.Export("svc_ip", svc)
+
+		return nil
+	})
+}
