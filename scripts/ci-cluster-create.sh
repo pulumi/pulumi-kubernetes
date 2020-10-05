9488f0980704460463a0b416abb2e8ce8d1e8710
stack72 <public@paulstack.co.uk>
2020-10-05 11:19:41 +0100

    Upload kubeconfig artifact for use in tests

--- scripts/ci-cluster-create.sh
+++ scripts/ci-cluster-create.sh
@@ -6,7 +6,7 @@ echo Creating ephemeral Kubernetes cluster for CI testing...
 pushd tests/ci-cluster
 yarn install
 pulumi stack init "${1}"
-pulumi up --skip-preview --yes
+pulumi up --skip-preview --yes --suppress-outputs
 
 mkdir -p "$HOME/.kube/"
 pulumi stack output kubeconfig >~/.kube/config
