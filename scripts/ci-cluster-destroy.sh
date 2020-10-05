5f57580969c13ebe8dc9a60c4bada0a4ffd76b5a
stack72 <public@paulstack.co.uk>
2020-10-05 11:31:26 +0100

    Yarn install as part of infra destroy to get the plugins

--- scripts/ci-cluster-destroy.sh
+++ scripts/ci-cluster-destroy.sh
@@ -4,6 +4,7 @@ set -o nounset -o errexit -o pipefail
 echo Deleting ephemeral Kubernetes cluster...
 
 pushd tests/ci-cluster
+yarn install
 pulumi stack select "${1}" && \
   pulumi destroy --skip-preview --yes && \
   pulumi stack rm --yes
