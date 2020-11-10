pulumi/pulumi-kubernetes

commit bab791ca34c3c9263d403c178c33364c0065879f

Add support for Create/Update previews. (#1355)
Implement support for the recently-added "provider preview" feature of
the Pulumi plugin interface that allows a resource provider to return
known values from a call to Create or Update during a preview. This
feature improves the usability of previews by allowing more values to be
treated as known.

When previewing a Create or Update operation, the Kuberentes provider
will either return the result of a dry-run performed by the API server
or will return the inputs as-is. The dry-run is preferred, but is only
possible when all of the inputs to the resource are known, the API
server supports dry-running the resource in question, and the user has
explicitly enabled the feature using the `enableDryRun` configuration
property. Returning the inputs as-is matches the behavior previously
implemented by the Pulumi engine.

provider/pkg/provider/provider.go
@@ -529,7 +529,8 @@ func (k *kubeProvider) Configure(_ context.Context, req *pulumirpc.ConfigureRequ
 	}
 
 	return &pulumirpc.ConfigureResponse{
-		AcceptSecrets: true,
+		AcceptSecrets:   true,
+		SupportsPreview: true,
 	}, nil
 }
 
@@ -1256,27 +1257,18 @@ func (k *kubeProvider) Diff(
 		oldInputs.SetGroupVersionKind(gvk)
 	}
 
-	supportsDryRun := false
-	if !k.clusterUnreachable {
-		supportsDryRun = openapi.SupportsDryRun(k.clientSet.DiscoveryClientCached, k.clientSet.GenericClient, gvk)
-	}
-
 	var patch []byte
 	var isClientSidePatch bool
 	var patchBase *unstructured.Unstructured
 
 	// Try to compute a server-side patch. Returns true iff the operation succeeded.
 	tryServerSidePatch := func() bool {
-		// If the configuration says to use client-side patch instead.
-		if !k.enableDryRun {
-			return false
-		}
-		// If the cluster does not support the server-side diff feature.
-		if !supportsDryRun {
-			return false
-		}
 		// If the resource's GVK changed, so compute patch using inputs.
 		if oldInputs.GroupVersionKind().String() != gvk.String() {
+			return false
+		}
+		// If we can't dry-run the new GVK, computed the patch using inputs.
+		if !k.supportsDryRun(gvk) {
 			return false
 		}
 		// TODO: Skipping server-side diff for resources with computed values is a hack. We will want to address this
@@ -1287,14 +1279,10 @@ func (k *kubeProvider) Diff(
 		}
 
 		patch, patchBase, err = k.serverSidePatch(oldInputs, newInputs)
+		if k.isDryRunDisabledError(err) {
+			return false
+		}
 		if se, isStatusError := err.(*errors.StatusError); isStatusError {
-			// If the cluster doesn't support server-side diff.
-			if se.Status().Code == http.StatusBadRequest &&
-				(se.Status().Message == "the dryRun alpha feature is disabled" ||
-					se.Status().Message == "the dryRun beta feature is disabled" ||
-					strings.Contains(se.Status().Message, "does not support dry run")) {
-				return false
-			}
 			// If the resource field is immutable.
 			if se.Status().Code == http.StatusUnprocessableEntity ||
 				strings.Contains(se.ErrStatus.Message, "field is immutable") {
@@ -1412,7 +1400,7 @@ func (k *kubeProvider) Create(
 
 	// Except in the case of yamlRender mode, Create requires a connection to a k8s cluster, so bail out
 	// immediately if it is unreachable.
-	if k.clusterUnreachable && !k.yamlRenderMode {
+	if !req.GetPreview() && k.clusterUnreachable && !k.yamlRenderMode {
 		return nil, fmt.Errorf("configured Kubernetes cluster is unreachable: %s", k.clusterUnreachableReason)
 	}
 
@@ -1436,6 +1424,18 @@ func (k *kubeProvider) Create(
 			err, "Failed to create resource %s/%s because of an error generating the %s value in "+
 				"`.metadata.annotations`",
 			newInputs.GetNamespace(), newInputs.GetName(), lastAppliedConfigKey)
+	}
+
+	// If this is a preview and the input values contain unknowns, return them as-is. This is compatible with
+	// prior behavior implemented by the Pulumi engine. Similarly, if the server does not support server-side
+	// dry run, return the inputs as-is.
+	if req.GetPreview() &&
+		(hasComputedValue(annotatedInputs) || !k.supportsDryRun(annotatedInputs.GroupVersionKind())) {
+
+		logger.V(9).Infof("cannot preview Create(%v)", urn)
+		return &pulumirpc.CreateResponse{
+			Id: fqObjName(annotatedInputs), Properties: req.GetProperties(),
+		}, nil
 	}
 
 	initialAPIVersion := newInputs.GetAPIVersion()
@@ -1487,9 +1487,17 @@ func (k *kubeProvider) Create(
 		},
 		Inputs:  annotatedInputs,
 		Timeout: req.Timeout,
+		DryRun:  req.GetPreview(),
 	}
 	initialized, awaitErr := await.Creation(config)
 	if awaitErr != nil {
+		if req.GetPreview() && k.isDryRunDisabledError(err) {
+			logger.V(9).Infof("could not preview Create(%v): %v", urn, err)
+			return &pulumirpc.CreateResponse{
+				Id: fqObjName(annotatedInputs), Properties: req.GetProperties(),
+			}, nil
+		}
+
 		if meta.IsNoMatchError(awaitErr) {
 			// If it's a "no match" error, this is probably a CustomResource with no corresponding
 			// CustomResourceDefinition. This usually happens if the CRD was not created, and we
@@ -1783,7 +1791,7 @@ func (k *kubeProvider) Update(
 
 	// Except in the case of yamlRender mode, Update requires a connection to a k8s cluster, so bail out
 	// immediately if it is unreachable.
-	if k.clusterUnreachable && !k.yamlRenderMode {
+	if !req.GetPreview() && k.clusterUnreachable && !k.yamlRenderMode {
 		return nil, fmt.Errorf("configured Kubernetes cluster is unreachable: %s", k.clusterUnreachableReason)
 	}
 
@@ -1816,6 +1824,16 @@ func (k *kubeProvider) Update(
 			err, "Failed to update resource %s/%s because of an error generating the %s value in "+
 				"`.metadata.annotations`",
 			newInputs.GetNamespace(), newInputs.GetName(), lastAppliedConfigKey)
+	}
+
+	// If this is a preview and the input values contain unknowns, return them as-is. This is compatible with
+	// prior behavior implemented by the Pulumi engine. Similarly, if the server does not support server-side
+	// dry run, return the inputs as-is.
+	if req.GetPreview() &&
+		(hasComputedValue(annotatedInputs) || !k.supportsDryRun(annotatedInputs.GroupVersionKind())) {
+
+		logger.V(9).Infof("cannot preview Update(%v)", urn)
+		return &pulumirpc.UpdateResponse{Properties: req.News}, nil
 	}
 
 	initialAPIVersion, err := initialAPIVersion(oldState, oldInputs)
@@ -1869,10 +1887,16 @@ func (k *kubeProvider) Update(
 		Previous: oldInputs,
 		Inputs:   annotatedInputs,
 		Timeout:  req.Timeout,
+		DryRun:   req.GetPreview(),
 	}
 	// Apply update.
 	initialized, awaitErr := await.Update(config)
 	if awaitErr != nil {
+		if req.GetPreview() && k.isDryRunDisabledError(err) {
+			logger.V(9).Infof("could not preview Update(%v): %v", urn, err)
+			return &pulumirpc.UpdateResponse{Properties: req.News}, nil
+		}
+
 		if meta.IsNoMatchError(awaitErr) {
 			// If it's a "no match" error, this is probably a CustomResource with no corresponding
 			// CustomResourceDefinition. This usually happens if the CRD was not created, and we
@@ -2158,6 +2182,32 @@ func (k *kubeProvider) inputPatch(
 	}
 
 	return jsonpatch.CreateMergePatch(oldInputsJSON, newInputsJSON)
+}
+
+func (k *kubeProvider) supportsDryRun(gvk schema.GroupVersionKind) bool {
+	// Check to see if the configuration has explicitly disabled server-side dry run.
+	if !k.enableDryRun {
+		logger.V(9).Infof("dry run is disabled")
+		return false
+	}
+	// Ensure that the cluster is reachable and supports the server-side diff feature.
+	if k.clusterUnreachable || !openapi.SupportsDryRun(k.clientSet.DiscoveryClientCached, k.clientSet.GenericClient, gvk) {
+		logger.V(9).Infof("server cannot dry run %v", gvk)
+		return false
+	}
+	return true
+}
+
+func (k *kubeProvider) isDryRunDisabledError(err error) bool {
+	se, isStatusError := err.(*errors.StatusError)
+	if !isStatusError {
+		return false
+	}
+
+	return se.Status().Code == http.StatusBadRequest &&
+		(se.Status().Message == "the dryRun alpha feature is disabled" ||
+			se.Status().Message == "the dryRun beta feature is disabled" ||
+			strings.Contains(se.Status().Message, "does not support dry run"))
 }
 
 func mapReplStripSecrets(v resource.PropertyValue) (interface{}, bool) {

provider/go.mod
@@ -11,8 +11,8 @@ require (
 	github.com/onsi/ginkgo v1.12.0 // indirect
 	github.com/onsi/gomega v1.9.0 // indirect
 	github.com/pkg/errors v0.9.1
-	github.com/pulumi/pulumi/pkg/v2 v2.11.2
-	github.com/pulumi/pulumi/sdk/v2 v2.11.2
+	github.com/pulumi/pulumi/pkg/v2 v2.11.3-0.20201012185126-156aa9862e15
+	github.com/pulumi/pulumi/sdk/v2 v2.11.3-0.20201012185126-156aa9862e15
 	github.com/stretchr/testify v1.6.1
 	google.golang.org/grpc v1.29.1
 	helm.sh/helm/v3 v3.3.3

provider/go.sum
@@ -879,11 +879,11 @@ github.com/prometheus/procfs v0.0.5/go.mod h1:4A/X28fw3Fc593LaREMrKMqOKvUAntwMDa
 github.com/prometheus/procfs v0.0.8 h1:+fpWZdT24pJBiqJdAwYBjPSk+5YmQzYNPYzQsdzLkt8=
 github.com/prometheus/procfs v0.0.8/go.mod h1:7Qr8sr6344vo1JqZ6HhLceV9o3AJ1Ff+GxbHq6oeK9A=
 github.com/prometheus/tsdb v0.7.1/go.mod h1:qhTCs0VvXwvX/y3TZrWD7rabWM+ijKTux40TwIPHuXU=
-github.com/pulumi/pulumi/pkg/v2 v2.11.2 h1:oXo5nARW17+jX7Qp9HvzLx3x+PZ2vlbLl56C/pbHFow=
-github.com/pulumi/pulumi/pkg/v2 v2.11.2/go.mod h1:zQWe2D4tYJDeXNzSclqNmP8/SMSKOh8k22AbWg3+mVc=
+github.com/pulumi/pulumi/pkg/v2 v2.11.3-0.20201012185126-156aa9862e15 h1:N4+VC5U8weg6k9DeJhX2GdOTaxgzjzG5JU0BjRIDlnM=
+github.com/pulumi/pulumi/pkg/v2 v2.11.3-0.20201012185126-156aa9862e15/go.mod h1:7NmgAy555Lk7v4Ddb6HF1BVAhdyT3012h47vz/hLD2k=
 github.com/pulumi/pulumi/sdk/v2 v2.2.1/go.mod h1:QNbWpL4gvf3X0lUFT7TXA2Jo1ff/Ti2l97AyFGYwvW4=
-github.com/pulumi/pulumi/sdk/v2 v2.11.2 h1:mqLuPm4Gv7VCXf1F71Mv+NnDZswmmiDepqFnfnxMtX4=
-github.com/pulumi/pulumi/sdk/v2 v2.11.2/go.mod h1:EED7KCDOohYIewUppsav5KHTFTmfYGqUFib1uRvYdWQ=
+github.com/pulumi/pulumi/sdk/v2 v2.11.3-0.20201012185126-156aa9862e15 h1:ivePIkhn9MsrBSJFqYVJKf03BxWVWN5WrSLQL7SJ9zM=
+github.com/pulumi/pulumi/sdk/v2 v2.11.3-0.20201012185126-156aa9862e15/go.mod h1:WQ4WaHMA7mduVHAJi87iIqbWvqsuBUYccBiKK+FrayI=
 github.com/qri-io/starlib v0.4.2-0.20200213133954-ff2e8cd5ef8d/go.mod h1:7DPO4domFU579Ga6E61sB9VFNaniPVwJP5C4bBCu3wA=
 github.com/quasilyte/go-consistent v0.0.0-20190521200055-c6f3937de18c/go.mod h1:5STLWrekHfjyYwxBRVRXNOSewLJ3PWfDJd1VyTS21fI=
 github.com/rcrowley/go-metrics v0.0.0-20181016184325-3113b8401b8a/go.mod h1:bCqnVzQkZxMG4s8nGwiZ5l3QUCyqpo9Y+/ZMZ9VjZe4=

provider/pkg/await/await.go
@@ -66,6 +66,7 @@ type CreateConfig struct {
 	ProviderConfig
 	Inputs  *unstructured.Unstructured
 	Timeout float64
+	DryRun  bool
 }
 
 type ReadConfig struct {
@@ -79,6 +80,7 @@ type UpdateConfig struct {
 	Previous *unstructured.Unstructured
 	Inputs   *unstructured.Unstructured
 	Timeout  float64
+	DryRun   bool
 }
 
 type DeleteConfig struct {
@@ -130,6 +132,11 @@ func Creation(c CreateConfig) (*unstructured.Unstructured, error) {
 	// nolint
 	// https://github.com/kubernetes/kubernetes/blob/54889d581a35acf940d52a8a384cccaa0b597ddc/pkg/kubectl/cmd/apply/apply.go#L94
 
+	var options metav1.CreateOptions
+	if c.DryRun {
+		options.DryRun = []string{metav1.DryRunAll}
+	}
+
 	var outputs *unstructured.Unstructured
 	var client dynamic.ResourceInterface
 	err := retry.SleepingRetry(
@@ -148,7 +155,7 @@ func Creation(c CreateConfig) (*unstructured.Unstructured, error) {
 				}
 			}
 
-			outputs, err = client.Create(context.TODO(), c.Inputs, metav1.CreateOptions{})
+			outputs, err = client.Create(context.TODO(), c.Inputs, options)
 			if err != nil {
 				_ = c.Host.LogStatus(c.Context, diag.Info, c.URN, fmt.Sprintf(
 					"Retry #%d; creation failed: %v", i, err))
@@ -166,6 +173,10 @@ func Creation(c CreateConfig) (*unstructured.Unstructured, error) {
 		return nil, err
 	}
 	_ = clearStatus(c.Context, c.Host, c.URN)
+
+	if c.DryRun {
+		return outputs, nil
+	}
 
 	// Wait until create resolves as success or error. Note that the conditional is set up to log
 	// only if we don't have an entry for the resource type; in the event that we do, but the await
@@ -335,12 +346,20 @@ func Update(c UpdateConfig) (*unstructured.Unstructured, error) {
 		return nil, err
 	}
 
+	var options metav1.PatchOptions
+	if c.DryRun {
+		options.DryRun = []string{metav1.DryRunAll}
+	}
+
 	// Issue patch request.
 	// NOTE: We can use the same client because if the `kind` changes, this will cause
 	// a replace (i.e., destroy and create).
-	currentOutputs, err := client.Patch(context.TODO(), c.Inputs.GetName(), patchType, patch, metav1.PatchOptions{})
+	currentOutputs, err := client.Patch(context.TODO(), c.Inputs.GetName(), patchType, patch, options)
 	if err != nil {
 		return nil, err
+	}
+	if c.DryRun {
+		return currentOutputs, nil
 	}
 
 	// Wait until patch resolves as success or error. Note that the conditional is set up to log only

commit a12cedc0c4b1c520d9390d8caf08467233de6028

Fix panic with computed values on preview
This change fixes a panic introduced in #1355. The panic
occurred in cases where the provider attempted to set the
last applied config annotation with computed values.

provider/pkg/provider/provider.go
@@ -1418,24 +1418,24 @@ func (k *kubeProvider) Create(
 
 	newInputs := propMapToUnstructured(newResInputs)
 
+	// If this is a preview and the input values contain unknowns, return them as-is. This is compatible with
+	// prior behavior implemented by the Pulumi engine. Similarly, if the server does not support server-side
+	// dry run, return the inputs as-is.
+	if req.GetPreview() &&
+		(hasComputedValue(newInputs) || !k.supportsDryRun(newInputs.GroupVersionKind())) {
+
+		logger.V(9).Infof("cannot preview Create(%v)", urn)
+		return &pulumirpc.CreateResponse{
+			Id: fqObjName(newInputs), Properties: req.GetProperties(),
+		}, nil
+	}
+
 	annotatedInputs, err := withLastAppliedConfig(newInputs)
 	if err != nil {
 		return nil, pkgerrors.Wrapf(
 			err, "Failed to create resource %s/%s because of an error generating the %s value in "+
 				"`.metadata.annotations`",
 			newInputs.GetNamespace(), newInputs.GetName(), lastAppliedConfigKey)
-	}
-
-	// If this is a preview and the input values contain unknowns, return them as-is. This is compatible with
-	// prior behavior implemented by the Pulumi engine. Similarly, if the server does not support server-side
-	// dry run, return the inputs as-is.
-	if req.GetPreview() &&
-		(hasComputedValue(annotatedInputs) || !k.supportsDryRun(annotatedInputs.GroupVersionKind())) {
-
-		logger.V(9).Infof("cannot preview Create(%v)", urn)
-		return &pulumirpc.CreateResponse{
-			Id: fqObjName(annotatedInputs), Properties: req.GetProperties(),
-		}, nil
 	}
 
 	initialAPIVersion := newInputs.GetAPIVersion()