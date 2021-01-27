b73966484f4101cb6768416fcbc02e24c3f5db21
Levi Blackstone <levi@pulumi.com>
2021-01-27 14:30:39 -0700

    Update tests

--- tests/sdk/go/basic/main.go
+++ tests/sdk/go/basic/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes"
 	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apiextensions"
-	apiextensionsv1b1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apiextensions/v1beta1"
+	apiextensionsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apiextensions/v1"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -25,70 +25,43 @@ func main() {
 			return err
 		}
 
-		_, err = apiextensionsv1b1.NewCustomResourceDefinition(ctx, "crd",
-			&apiextensionsv1b1.CustomResourceDefinitionArgs{
+		_, err = apiextensionsv1.NewCustomResourceDefinition(ctx, "crd",
+			&apiextensionsv1.CustomResourceDefinitionArgs{
 				Metadata: metav1.ObjectMetaArgs{
 					Name: pulumi.String("tests.example.com"),
 				},
-				Spec: apiextensionsv1b1.CustomResourceDefinitionSpecArgs{
+				Spec: apiextensionsv1.CustomResourceDefinitionSpecArgs{
 					Group: pulumi.String("example.com"),
-					Versions: apiextensionsv1b1.CustomResourceDefinitionVersionArray{
-						apiextensionsv1b1.CustomResourceDefinitionVersionArgs{
+					Versions: apiextensionsv1.CustomResourceDefinitionVersionArray{
+						apiextensionsv1.CustomResourceDefinitionVersionArgs{
 							Name:    pulumi.String("v1"),
 							Served:  pulumi.Bool(true),
 							Storage: pulumi.Bool(true),
+							Schema: apiextensionsv1.CustomResourceValidationArgs{
+								OpenAPIV3Schema: apiextensionsv1.JSONSchemaPropsArgs{
+									Type: pulumi.String("object"),
+									Properties: apiextensionsv1.JSONSchemaPropsMap{
+										"spec": apiextensionsv1.JSONSchemaPropsArgs{
+											Type: pulumi.String("object"),
+											Properties: apiextensionsv1.JSONSchemaPropsMap{
+												"foo": apiextensionsv1.JSONSchemaPropsArgs{
+													Type: pulumi.String("string"),
+												},
+											},
+										},
+									},
+								},
+							},
 						},
 					},
 					Scope: pulumi.String("Cluster"),
-					Names: apiextensionsv1b1.CustomResourceDefinitionNamesArgs{
+					Names: apiextensionsv1.CustomResourceDefinitionNamesArgs{
 						Plural:   pulumi.String("tests"),
 						Singular: pulumi.String("test"),
 						Kind:     pulumi.String("Test"),
 					},
 				},
 			})
-		if err != nil {
-			return err
-		}
-
-		// TODO: Switch to the v1 API once https://github.com/pulumi/pulumi-kubernetes/issues/1128 is done.
-		//_, err = apiextensionsv1.NewCustomResourceDefinition(ctx, "crd",
-		//	&apiextensionsv1.CustomResourceDefinitionArgs{
-		//		Metadata: metav1.ObjectMetaArgs{
-		//			Name: pulumi.String("tests.example.com"),
-		//		},
-		//		Spec: apiextensionsv1.CustomResourceDefinitionSpecArgs{
-		//			Group: pulumi.String("example.com"),
-		//			Versions: apiextensionsv1.CustomResourceDefinitionVersionArray{
-		//				apiextensionsv1.CustomResourceDefinitionVersionArgs{
-		//					Name:    pulumi.String("v1"),
-		//					Served:  pulumi.Bool(true),
-		//					Storage: pulumi.Bool(true),
-		//					Schema: apiextensionsv1.CustomResourceValidationArgs{
-		//						OpenAPIV3Schema: apiextensionsv1.JSONSchemaPropsArgs{
-		//							Type: pulumi.String("object"),
-		//							Properties: apiextensionsv1.JSONSchemaPropsMap{
-		//								"spec": apiextensionsv1.JSONSchemaPropsArgs{
-		//									Type: pulumi.String("object"),
-		//									Properties: apiextensionsv1.JSONSchemaPropsMap{
-		//										"foo": apiextensionsv1.JSONSchemaPropsArgs{
-		//											Type: pulumi.String("string"),
-		//										},
-		//									},
-		//								},
-		//							},
-		//						},
-		//					},
-		//				},
-		//			},
-		//			Scope: pulumi.String("Cluster"),
-		//			Names: apiextensionsv1.CustomResourceDefinitionNamesArgs{
-		//				Plural:   pulumi.String("tests"),
-		//				Singular: pulumi.String("test"),
-		//				Kind:     pulumi.String("Test"),
-		//			},
-		//		},
-		//	})
 
 		_, err = apiextensions.NewCustomResource(ctx, "cr", &apiextensions.CustomResourceArgs{
 			ApiVersion: pulumi.String("example.com/v1"),
