pulumi/pulumi-kubernetes

commit de129e33b01770d21be8f26a44a676f69d297785

Add replaceUnready annotation for Jobs (#1575)
Add the `pulumi.com/replaceUnready` annotation that allows
the user to specify that a Job resource should be replaced
during an update if it is not ready. This annotation could be
extended to support other resource types as well, but currently
only supports Jobs.

provider/pkg/provider/provider.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2021, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -35,6 +35,7 @@ import (
 	structpb "github.com/golang/protobuf/ptypes/struct"
 	pkgerrors "github.com/pkg/errors"
 	"github.com/pulumi/pulumi-kubernetes/provider/v3/pkg/await"
+	"github.com/pulumi/pulumi-kubernetes/provider/v3/pkg/await/states"
 	"github.com/pulumi/pulumi-kubernetes/provider/v3/pkg/clients"
 	"github.com/pulumi/pulumi-kubernetes/provider/v3/pkg/cluster"
 	"github.com/pulumi/pulumi-kubernetes/provider/v3/pkg/gen"
@@ -1365,6 +1366,23 @@ func (k *kubeProvider) Diff(
 			case pulumirpc.PropertyDiff_ADD_REPLACE, pulumirpc.PropertyDiff_DELETE_REPLACE, pulumirpc.PropertyDiff_UPDATE_REPLACE:
 				replaces = append(replaces, k)
 			}
+		}
+	}
+
+	if metadata.ReplaceUnready(newInputs) {
+		switch newInputs.GetKind() {
+		case "Job":
+			jobChecker := states.NewJobChecker()
+			job, err := clients.FromUnstructured(newInputs)
+			if err == nil {
+				jobChecker.Update(job)
+				if !jobChecker.Ready() {
+					hasChanges = pulumirpc.DiffResponse_DIFF_SOME
+					replaces = append(replaces, `.metadata.annotations["pulumi.com/replaceUnready"]`)
+				}
+			}
+		default:
+			_ = k.host.Log(ctx, diag.Warning, urn, "replaceUnready annotation is not supported for this resource")
 		}
 	}
 

provider/pkg/gen/additionalComments.go
@@ -147,6 +147,13 @@ https://kubernetes.io/docs/concepts/configuration/secret/#risks`
 	}
 }
 
+func replaceUnreadyComment() string {
+	return `By default, if a resource failed to become ready in a previous update, 
+Pulumi will continue to wait for readiness on the next update. If you would prefer
+to schedule a replacement for an unready resource on the next update, you can add the
+"pulumi.com/replaceUnready": "true" annotation to the resource definition.`
+}
+
 // PulumiComment adds additional information to the docs generated automatically from the OpenAPI specs.
 // This includes information about Pulumi's await behavior, deprecation information, etc.
 func PulumiComment(kind string) string {
@@ -154,8 +161,10 @@ func PulumiComment(kind string) string {
 
 	k := kinds.Kind(kind)
 	switch k {
-	case kinds.Deployment, kinds.Ingress, kinds.Job, kinds.Pod, kinds.Service, kinds.StatefulSet:
+	case kinds.Deployment, kinds.Ingress, kinds.Pod, kinds.Service, kinds.StatefulSet:
 		return prefix + awaitComments(k)
+	case kinds.Job:
+		return prefix + awaitComments(k) + prefix + replaceUnreadyComment()
 	case kinds.Secret:
 		return prefix + helpfulLinkComments(k)
 	default:

provider/pkg/metadata/annotations.go
@@ -31,6 +31,7 @@ const (
 	AnnotationSkipAwait         = AnnotationPrefix + "skipAwait"
 	AnnotationTimeoutSeconds    = AnnotationPrefix + "timeoutSeconds"
 	AnnotationInitialAPIVersion = AnnotationPrefix + "initialApiVersion"
+	AnnotationReplaceUnready    = AnnotationPrefix + "replaceUnready"
 
 	AnnotationHelmHook = "helm.sh/hook"
 )

provider/pkg/metadata/overrides.go
@@ -26,6 +26,11 @@ func SkipAwaitLogic(obj *unstructured.Unstructured) bool {
 	return IsAnnotationTrue(obj, AnnotationSkipAwait)
 }
 
+// ReplaceUnready returns true if the `pulumi.com/replaceUnready` annotation is "true", false otherwise.
+func ReplaceUnready(obj *unstructured.Unstructured) bool {
+	return IsAnnotationTrue(obj, AnnotationReplaceUnready)
+}
+
 // TimeoutDuration returns the resource timeout duration. There are a number of things it can do here in this order
 // 1. Return the timeout as specified in the customResource options
 // 2. Return the timeout as specified in `pulumi.com/timeoutSeconds` annotation,

sdk/dotnet/Batch/V1/Job.cs
@@ -26,6 +26,11 @@ namespace Pulumi.Kubernetes.Batch.V1
     /// If the Job has not reached a Ready state after 10 minutes, it will
     /// time out and mark the resource update as Failed. You can override the default timeout value
     /// by setting the 'customTimeouts' option on the resource.
+    /// 
+    /// By default, if a resource failed to become ready in a previous update,
+    /// Pulumi will continue to wait for readiness on the next update. If you would prefer
+    /// to schedule a replacement for an unready resource on the next update, you can add the
+    /// "pulumi.com/replaceUnready": "true" annotation to the resource definition.
     /// </summary>
     [KubernetesResourceType("kubernetes:batch/v1:Job")]
     public partial class Job : KubernetesResource

sdk/go/kubernetes/batch/v1/job.go
@@ -27,6 +27,11 @@ import (
 // If the Job has not reached a Ready state after 10 minutes, it will
 // time out and mark the resource update as Failed. You can override the default timeout value
 // by setting the 'customTimeouts' option on the resource.
+//
+// By default, if a resource failed to become ready in a previous update,
+// Pulumi will continue to wait for readiness on the next update. If you would prefer
+// to schedule a replacement for an unready resource on the next update, you can add the
+// "pulumi.com/replaceUnready": "true" annotation to the resource definition.
 type Job struct {
 	pulumi.CustomResourceState
 

sdk/go/kubernetes/batch/v1/pulumiTypes.go
@@ -666,6 +666,11 @@ func (o CronJobStatusPtrOutput) LastSuccessfulTime() pulumi.StringPtrOutput {
 // If the Job has not reached a Ready state after 10 minutes, it will
 // time out and mark the resource update as Failed. You can override the default timeout value
 // by setting the 'customTimeouts' option on the resource.
+//
+// By default, if a resource failed to become ready in a previous update,
+// Pulumi will continue to wait for readiness on the next update. If you would prefer
+// to schedule a replacement for an unready resource on the next update, you can add the
+// "pulumi.com/replaceUnready": "true" annotation to the resource definition.
 type JobType struct {
 	// APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
 	ApiVersion *string `pulumi:"apiVersion"`
@@ -706,6 +711,11 @@ type JobTypeInput interface {
 // If the Job has not reached a Ready state after 10 minutes, it will
 // time out and mark the resource update as Failed. You can override the default timeout value
 // by setting the 'customTimeouts' option on the resource.
+//
+// By default, if a resource failed to become ready in a previous update,
+// Pulumi will continue to wait for readiness on the next update. If you would prefer
+// to schedule a replacement for an unready resource on the next update, you can add the
+// "pulumi.com/replaceUnready": "true" annotation to the resource definition.
 type JobTypeArgs struct {
 	// APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
 	ApiVersion pulumi.StringPtrInput `pulumi:"apiVersion"`
@@ -772,6 +782,11 @@ func (i JobTypeArray) ToJobTypeArrayOutputWithContext(ctx context.Context) JobTy
 // If the Job has not reached a Ready state after 10 minutes, it will
 // time out and mark the resource update as Failed. You can override the default timeout value
 // by setting the 'customTimeouts' option on the resource.
+//
+// By default, if a resource failed to become ready in a previous update,
+// Pulumi will continue to wait for readiness on the next update. If you would prefer
+// to schedule a replacement for an unready resource on the next update, you can add the
+// "pulumi.com/replaceUnready": "true" annotation to the resource definition.
 type JobTypeOutput struct{ *pulumi.OutputState }
 
 func (JobTypeOutput) ElementType() reflect.Type {

sdk/nodejs/batch/v1/job.ts
@@ -22,6 +22,11 @@ import * as utilities from "../../utilities";
  * If the Job has not reached a Ready state after 10 minutes, it will
  * time out and mark the resource update as Failed. You can override the default timeout value
  * by setting the 'customTimeouts' option on the resource.
+ *
+ * By default, if a resource failed to become ready in a previous update,
+ * Pulumi will continue to wait for readiness on the next update. If you would prefer
+ * to schedule a replacement for an unready resource on the next update, you can add the
+ * "pulumi.com/replaceUnready": "true" annotation to the resource definition.
  */
 export class Job extends pulumi.CustomResource {
     /**

commit d4292c780afba3fe4763b2f6ec73c64a7845d27f

Read live Job state for replaceUnready check
The check added in de129e3 mistakenly did not check the
live status of the Job, but instead checked the user inputs.
This would lead to erroneous replacements for ready Jobs.

provider/pkg/provider/provider.go
@@ -1372,13 +1372,16 @@ func (k *kubeProvider) Diff(
 	if metadata.ReplaceUnready(newInputs) {
 		switch newInputs.GetKind() {
 		case "Job":
-			jobChecker := states.NewJobChecker()
-			job, err := clients.FromUnstructured(newInputs)
-			if err == nil {
-				jobChecker.Update(job)
-				if !jobChecker.Ready() {
-					hasChanges = pulumirpc.DiffResponse_DIFF_SOME
-					replaces = append(replaces, `.metadata.annotations["pulumi.com/replaceUnready"]`)
+			// Fetch current Job status and check point-in-time readiness. Errors are ignored.
+			if live, err := k.readLiveObject(newInputs); err == nil {
+				jobChecker := states.NewJobChecker()
+				job, err := clients.FromUnstructured(live)
+				if err == nil {
+					jobChecker.Update(job)
+					if !jobChecker.Ready() {
+						hasChanges = pulumirpc.DiffResponse_DIFF_SOME
+						replaces = append(replaces, `.metadata.annotations["pulumi.com/replaceUnready"]`)
+					}
 				}
 			}
 		default: