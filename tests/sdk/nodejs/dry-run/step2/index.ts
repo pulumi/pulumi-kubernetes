31317642784f55400496bc4c19cec3f6eaa4b962
Levi Blackstone <levi@pulumi.com>
2021-06-22 14:55:13 -0600

    Test

--- tests/sdk/nodejs/dry-run/step2/index.ts
+++ tests/sdk/nodejs/dry-run/step2/index.ts
@@ -19,7 +19,7 @@ import * as pulumi from "@pulumi/pulumi";
 // TODO(levi): Use auto-naming for namespace once https://github.com/pulumi/pulumi-kubernetes/issues/1632 is fixed.
 const suffix = new random.RandomString("suffix", {length: 7, special: false, upper: false}).result;
 const ns = new k8s.core.v1.Namespace("test", {metadata: {name: pulumi.interpolate`test-${suffix}`}});
-const provider = new k8s.Provider("k8s", {enableDryRun: true, namespace: ns.metadata.name});
+const provider = new k8s.Provider("k8s", {enableDryRun: true}); // Use the default namespace.
 
 const appLabels = { app: "nginx" };
 const deployment = new k8s.apps.v1.Deployment("nginx", {
