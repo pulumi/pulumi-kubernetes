3409dac44acbef589290de9b7590395893a410ba
Vivek Lakshmanan <vivek@lakshmanan.ca>
2021-08-12 14:05:29 -0700

    Nodejs

--- sdk/nodejs/types/input.ts
+++ sdk/nodejs/types/input.ts
@@ -12154,6 +12154,176 @@ export namespace flowcontrol {
     }
 }
 
+export namespace helm {
+    export namespace v3 {
+        /**
+         * Specification defining the Helm Release to install.
+         */
+        export interface ReleaseSpec {
+            /**
+             * If set, installation process purges chart on fail. The wait flag will be set automatically if atomic is used
+             */
+            atomic?: pulumi.Input<boolean>;
+            /**
+             * Chart name to be installed. A path may be used.
+             */
+            chart: pulumi.Input<string>;
+            /**
+             * Allow deletion of new resources created in this upgrade when upgrade fails
+             */
+            cleanupOnFail?: pulumi.Input<boolean>;
+            /**
+             * Create the namespace if it does not exist
+             */
+            createNamespace?: pulumi.Input<boolean>;
+            /**
+             * Run helm dependency update before installing the chart
+             */
+            dependencyUpdate?: pulumi.Input<boolean>;
+            /**
+             * Add a custom description
+             */
+            description?: pulumi.Input<string>;
+            /**
+             * Use chart development versions, too. Equivalent to version '>0.0.0-0'. If `version` is set, this is ignored
+             */
+            devel?: pulumi.Input<boolean>;
+            /**
+             * Prevent CRD hooks from, running, but run other hooks.  See helm install --no-crd-hook
+             */
+            disableCRDHooks?: pulumi.Input<boolean>;
+            /**
+             * If set, the installation process will not validate rendered templates against the Kubernetes OpenAPI Schema
+             */
+            disableOpenapiValidation?: pulumi.Input<boolean>;
+            /**
+             * Prevent hooks from running.
+             */
+            disableWebhooks?: pulumi.Input<boolean>;
+            /**
+             * Force resource update through delete/recreate if needed.
+             */
+            forceUpdate?: pulumi.Input<boolean>;
+            /**
+             * Location of public keys used for verification. Used only if `verify` is true
+             */
+            keyring?: pulumi.Input<string>;
+            /**
+             * Run helm lint when planning
+             */
+            lint?: pulumi.Input<boolean>;
+            /**
+             * Limit the maximum number of revisions saved per release. Use 0 for no limit
+             */
+            maxHistory?: pulumi.Input<number>;
+            /**
+             * Release name.
+             */
+            name: pulumi.Input<string>;
+            /**
+             * Namespace to install the release into.
+             */
+            namespace?: pulumi.Input<string>;
+            /**
+             * Postrender command to run.
+             */
+            postrender?: pulumi.Input<string>;
+            /**
+             * Perform pods restart during upgrade/rollback
+             */
+            recreatePods?: pulumi.Input<boolean>;
+            /**
+             * If set, render subchart notes along with the parent
+             */
+            renderSubchartNotes?: pulumi.Input<boolean>;
+            /**
+             * Re-use the given name, even if that name is already used. This is unsafe in production
+             */
+            replace?: pulumi.Input<boolean>;
+            /**
+             * Specification defining the Helm chart repository to use.
+             */
+            repositorySpec: pulumi.Input<inputs.helm.v3.RepositorySpec>;
+            /**
+             * When upgrading, reset the values to the ones built into the chart
+             */
+            resetValues?: pulumi.Input<boolean>;
+            /**
+             * When upgrading, reuse the last release's values and merge in any overrides. If 'reset_values' is specified, this is ignored
+             */
+            reuseValues?: pulumi.Input<boolean>;
+            /**
+             * Custom values to be merged with the values.
+             */
+            set: pulumi.Input<pulumi.Input<inputs.helm.v3.SetValue>[]>;
+            /**
+             * If set, no CRDs will be installed. By default, CRDs are installed if not already present
+             */
+            skipCrds?: pulumi.Input<boolean>;
+            /**
+             * Time in seconds to wait for any individual kubernetes operation.
+             */
+            timeout?: pulumi.Input<number>;
+            /**
+             * List of values in raw yaml format to pass to helm.
+             */
+            values?: pulumi.Input<pulumi.Input<string>[]>;
+            /**
+             * Verify the package before installing it.
+             */
+            verify?: pulumi.Input<boolean>;
+            /**
+             * Specify the exact chart version to install. If this is not specified, the latest version is installed.
+             */
+            version?: pulumi.Input<string>;
+            /**
+             * Will wait until all resources are in a ready state before marking the release as successful.
+             */
+            wait?: pulumi.Input<boolean>;
+            /**
+             * If wait is enabled, will wait until all Jobs have been completed before marking the release as successful.
+             */
+            waitForJobs?: pulumi.Input<boolean>;
+        }
+
+        /**
+         * Specification defining the Helm chart repository to use.
+         */
+        export interface RepositorySpec {
+            /**
+             * Repository where to locate the requested chart. If is a URL the chart is installed without installing the repository.
+             */
+            repository?: pulumi.Input<string>;
+            /**
+             * The Repositories CA File
+             */
+            repositoryCAFile?: pulumi.Input<string>;
+            /**
+             * The repositories cert file
+             */
+            repositoryCertFile?: pulumi.Input<string>;
+            /**
+             * The repositories cert key file
+             */
+            repositoryKeyFile?: pulumi.Input<string>;
+            /**
+             * Password for HTTP basic authentication
+             */
+            repositoryPassword?: pulumi.Input<string>;
+            /**
+             * Username for HTTP basic authentication
+             */
+            repositoryUsername?: pulumi.Input<string>;
+        }
+
+        export interface SetValue {
+            name: pulumi.Input<string>;
+            type?: pulumi.Input<string>;
+            value: pulumi.Input<string>;
+        }
+    }
+}
+
 export namespace meta {
     export namespace v1 {
         /**
