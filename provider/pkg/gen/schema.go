a1ae5376cc8600899d925277997a78051fc3b5ba
Levi Blackstone <levi@pulumi.com>
2021-08-20 15:43:03 -0600

    Update docstring for suppressHelmHookWarnings flag

--- provider/pkg/gen/schema.go
+++ provider/pkg/gen/schema.go
@@ -137,7 +137,7 @@ func PulumiSchema(swagger map[string]interface{}) pschema.PackageSpec {
 							"PULUMI_K8S_SUPPRESS_HELM_HOOK_WARNINGS",
 						},
 					},
-					Description: "If present and set to true, suppress unsupported Helm hook warnings from the CLI.\n\nThis config can be specified in the following ways, using this precedence:\n1. This `suppressHelmHookWarnings` parameter.\n2. The `PULUMI_K8S_SUPPRESS_HELM_HOOK_WARNINGS` environment variable.",
+					Description: "If present and set to true, suppress unsupported Helm hook warnings from the CLI.",
 					TypeSpec:    pschema.TypeSpec{Type: "boolean"},
 				},
 			},
