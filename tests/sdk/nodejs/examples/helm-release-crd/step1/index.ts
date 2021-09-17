30e59e7d596eea4580ed50765fbb081081daddf9
Vivek Lakshmanan <vivek@lakshmanan.ca>
2021-09-17 09:35:40 -0700

    Pin versions

--- tests/sdk/nodejs/examples/helm-release-crd/step1/index.ts
+++ tests/sdk/nodejs/examples/helm-release-crd/step1/index.ts
@@ -3,6 +3,7 @@ import * as k8s from "@pulumi/kubernetes";
 const prometheusRelease = new k8s.helm.v3.Release("prometheus", {
     name: "kube-prometheus-stack",
     chart: "kube-prometheus-stack",
+    version: "18.0.10",
     repositoryOpts: {
         repo: "https://prometheus-community.github.io/helm-charts",
     },
