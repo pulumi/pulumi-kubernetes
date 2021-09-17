39ee59e8d98dda016e1230d9f08e7e79e80b4563
Vivek Lakshmanan <vivek@lakshmanan.ca>
2021-09-17 16:13:10 -0700

    Switch to an unused helm chart with CRDs

--- tests/sdk/nodejs/examples/helm-release-crd/step1/index.ts
+++ tests/sdk/nodejs/examples/helm-release-crd/step1/index.ts
@@ -1,11 +1,11 @@
 import * as k8s from "@pulumi/kubernetes";
 
-const prometheusRelease = new k8s.helm.v3.Release("prometheus", {
-    name: "kube-prometheus-stack",
-    chart: "kube-prometheus-stack",
-    version: "18.0.10",
+const dapr = new k8s.helm.v3.Release("dapr", {
+    name: "dapr",
+    chart: "dapr",
+    version: "1.4.0",
     repositoryOpts: {
-        repo: "https://prometheus-community.github.io/helm-charts",
+        repo: "https://dapr.github.io/helm-charts/",
     },
     values: {},
 });
