cb2803c54ec2131c04564f863dd3577284fa1650
Vivek Lakshmanan <vivek@pulumi.com>
2021-12-21 22:01:19 -0500

    Exclude nil entries from values for helm charts (#1845)
    * Exclude nil entries from values
    
    * Changelog
    
    * Fix IsNil handling

---
+++ provider/pkg/provider/helm_release_test.go
@@ -0,0 +1,97 @@
+package provider
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func Test_MergeMaps(t *testing.T) {
+	m := map[string]interface{}{
+		"a": map[string]interface{}{
+			"b": map[string]interface{}{
+				"d": []interface{}{
+					"1", "2",
+				},
+			},
+		},
+	}
+
+	override := map[string]interface{}{
+		"a": map[string]interface{}{
+			"b": map[string]interface{}{
+				"d": []interface{}{
+					"3", "4",
+				},
+			},
+		},
+	}
+
+	for _, test := range []struct {
+		name     string
+		dest     map[string]interface{}
+		src      map[string]interface{}
+		expected map[string]interface{}
+	}{
+		{
+			name:     "Precedence",
+			dest:     m,
+			src:      override,
+			expected: override, // Expect the override to take precedence
+		},
+		{
+			name: "Merge maps",
+			dest: m,
+			src: map[string]interface{}{
+				"a": map[string]interface{}{
+					"b": map[string]interface{}{
+						"c": []interface{}{
+							"3", "4",
+						},
+						"f": true,
+					},
+				},
+			},
+			expected: map[string]interface{}{
+				"a": map[string]interface{}{
+					"b": map[string]interface{}{
+						"d": []interface{}{
+							"1", "2",
+						},
+						"c": []interface{}{
+							"3", "4",
+						},
+						"f": true,
+					},
+				},
+			},
+		},
+		{
+			name: "Dest Has Nil Values",
+			dest: m,
+			src: map[string]interface{}{
+				"a": map[string]interface{}{
+					"b": map[string]interface{}{
+						"c": interface{}(nil),
+						"e": (*interface{})(nil),
+					},
+				},
+			},
+			expected: map[string]interface{}{
+				"a": map[string]interface{}{
+					"b": map[string]interface{}{
+						"d": []interface{}{
+							"1", "2",
+						},
+					},
+				},
+			},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			merged, err := mergeMaps(test.dest, test.src)
+			require.NoError(t, err)
+			assert.Equal(t, test.expected, merged)
+		})
+	}
+}
