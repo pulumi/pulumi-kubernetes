4ed22baaf9ad70801c3d89b333f38ffbe75773d9
Levi Blackstone <levi@pulumi.com>
2022-06-07 10:25:34 -0600

    .NET tests

--- tests/sdk/dotnet/kustomize/Program.cs
+++ tests/sdk/dotnet/kustomize/Program.cs
@@ -1,17 +1,22 @@
-﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2022, Pulumi Corporation.  All rights reserved.
 
 using System;
 using System.Threading.Tasks;
 using Pulumi;
+using Pulumi.Kubernetes;
 using Pulumi.Kubernetes.Kustomize;
 
 class KustomizeStack : Stack
 {
     public KustomizeStack()
     {
+        var provider = new Provider("k8s");
         var files = new Directory("helloWorld", new DirectoryArgs
         {
             Directory = "helloWorld"
+        }, new ComponentResourceOptions
+        {
+            Provider = provider,
         });
     }
 
