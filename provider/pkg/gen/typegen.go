0b7456a8c57d56b503da29b3d41e6cbba199e6c9
Ramon Quitales <ramon@pulumi.com>
2023-05-16 12:47:59 -0700

    Remove SelfSubjectReview

--- provider/pkg/gen/typegen.go
+++ provider/pkg/gen/typegen.go
@@ -203,8 +203,8 @@ func (d definition) isTopLevel() bool {
 	switch fmt.Sprintf("%s/%s", d.gvk.GroupVersion().String(), d.gvk.Kind) {
 	case "policy/v1beta1/Eviction", "policy/v1/Eviction", "v1/Status", "apps/v1beta1/Scale", "apps/v1beta2/Scale",
 		"autoscaling/v1/Scale", "extensions/v1beta1/Scale", "core/v1/ComponentStatus", "core/v1/ComponentStatusList",
-		"authentication/v1/TokenRequest", "authentication/v1/TokenRequestPatch", "authentication/v1/TokenReview",
-		"authentication/v1/TokenReviewPatch":
+		"authentication/v1/TokenRequest", "authentication/v1/TokenReview", "authentication/v1beta1/TokenReview",
+		"authentication/v1beta1/SelfSubjectReview", "authentication/v1alpha1/SelfSubjectReview":
 		return false
 	}
 
