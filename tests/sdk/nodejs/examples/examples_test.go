87838f94016fb53afe4663c6a9d60e57a944a9b0
Levi Blackstone <levi@pulumi.com>
2023-08-11 12:31:41 -0600

    Use kubectl to validate

--- tests/sdk/nodejs/examples/examples_test.go
+++ tests/sdk/nodejs/examples/examples_test.go
@@ -25,6 +25,7 @@ import (
 	"time"
 
 	"github.com/pulumi/pulumi-kubernetes/provider/v4/pkg/openapi"
+	"github.com/pulumi/pulumi-kubernetes/tests/v4"
 	"github.com/pulumi/pulumi/pkg/v3/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
@@ -442,6 +443,11 @@ func TestHelmReleaseNamespace(t *testing.T) {
 					"expected Helm resources to reside in the provided Namespace")
 				assert.NotEmptyf(t, stackInfo.Outputs["alertManagerNamespace"].(string),
 					"Helm resources should not be in the default Namespace")
+				getResponse, err := tests.Kubectl(fmt.Sprintf("get statefulset -n %s alertmanager",
+					stackInfo.Outputs["alertManagerNamespace"]))
+				assert.NoErrorf(t, err, "kubectl command failed")
+				assert.NotContainsf(t, getResponse, "No resources found",
+					"kubectl did not find the expected resource")
 			},
 			EditDirs: []integration.EditDir{
 				{
@@ -453,6 +459,11 @@ func TestHelmReleaseNamespace(t *testing.T) {
 							"expected Helm resources to reside in the provided Namespace")
 						assert.NotEmptyf(t, stackInfo.Outputs["alertManagerNamespace"].(string),
 							"Helm resources should not be in the default Namespace")
+						getResponse, err := tests.Kubectl(fmt.Sprintf("get statefulset -n %s alertmanager",
+							stackInfo.Outputs["alertManagerNamespace"]))
+						assert.NoErrorf(t, err, "kubectl command failed")
+						assert.NotContainsf(t, getResponse, "No resources found",
+							"kubectl did not find the expected resource")
 					},
 				},
 			},
