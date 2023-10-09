522379d4e78db2fda7c5675c2e5bdd07c71e28cc
Henry Peteet <henry.peteet@freenome.com>
2023-10-09 13:16:39 -0400

    add dotnet tests for kubeversion (untested, local tests not runing)

---
+++ tests/sdk/dotnet/helm-kube-version/step1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Copyright 2016-2022, Pulumi Corporation.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using Pulumi;
+using Pulumi.Kubernetes;
+using Pulumi.Kubernetes.Core.V1;
+using Pulumi.Kubernetes.Helm;
+using Pulumi.Kubernetes.Helm.V3;
+using Pulumi.Kubernetes.Types.Inputs.Core.V1;
+using Pulumi.Kubernetes.Yaml;
+using Pulumi.Serialization;
+
+class HelmStack : Stack
+{
+    public HelmStack()
+    {
+        var provider = new Provider("k8s");
+        var namespaceTest = new Namespace("test", null, new CustomResourceOptions{Provider = provider});
+        var namespaceName = namespaceTest.Metadata.Apply(n => n.Name);
+      
+        new Chart("kube-version", new LocalChartArgs
+        {
+            KubeVersion = "1.25.0"
+            Namespace = namespaceName,
+            Path = "helm-kube-version"
+        }, new ComponentResourceOptions
+        {
+            Provider = provider,
+        });
+    }
+}
+
+class Program
+{
+    static Task<int> Main(string[] args) => Deployment.RunAsync<HelmStack>();
+}
