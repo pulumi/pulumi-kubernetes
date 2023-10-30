c56daaf77357a7f48518aee73b17b7dd87a76f89
Eron Wright <eron@pulumi.com>
2023-10-30 15:46:07 -0700

    Fix support on Windows for Helm Release w/ OCI

--- provider/pkg/provider/helm_release.go
+++ provider/pkg/provider/helm_release.go
@@ -1536,15 +1536,12 @@ func getChart(cpo *action.ChartPathOptions, registryClient *registry.Client, set
 func localChart(name string, verify bool, keyring string) (string, bool, error) {
 	fi, err := os.Stat(name)
 	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			return "", false, nil
-		}
-
-		return "", false, err
+		// Helm eats all errors at this point.
+		return "", false, nil
 	}
 
 	// If a folder is of the same name as a chart, use the folder if it contains a Chart.yaml.
-	if err == nil && fi.IsDir() {
+	if fi.IsDir() {
 		if _, err := os.Stat(filepath.Join(name, "Chart.yaml")); err != nil {
 			// This is not a chart directory, so do not error as Helm could still
 			// resolve this as a locally added chart repository, eg. `helm repo add`.
