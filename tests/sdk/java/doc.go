90910a65b5cb81c084a3b6051c99b1122b2a592c
Bryce Lampe <bryce@pulumi.com>
2024-05-09 10:46:52 -0700

    Fix a couple tests not running on CI (#2982)
    I noticed as part of
    https://github.com/pulumi/pulumi-kubernetes/pull/2953 that the
    `tests/convert` and `tests/provider` directories aren't currently
    getting exercised in CI, and as a result one of them is failing on
    master.
    
    This PR moves `tests/provider` and `tests/convert` under `sdk/java`,
    which is currently executed in CI but doesn't contain any tests. We also
    fix the convert test along the way.
    
    "sdk/java" is admittedly an odd namespace to put these under, but our CI
    targets don't give us much flexibility at the moment, and at the end of
    the day it's just an arbitrary way of sharding things.

--- tests/sdk/java/doc.go
+++ tests/sdk/java/doc.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2022, Pulumi Corporation.
+// Copyright 2024, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,4 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package test contains a number of end-to-end tests written in YAML despite
+// being located under sdk/java. This is necessary because our CI jobs don't
+// currently execute a test step for YAML. We don't have any Java SDK tests at
+// the moment so we might as well give this shard something to do.
+//
+// See https://github.com/pulumi/ci-mgmt/issues/676#issuecomment-2085892601 for
+// a discussion of how to better distribute these tests in CI.
 package test
