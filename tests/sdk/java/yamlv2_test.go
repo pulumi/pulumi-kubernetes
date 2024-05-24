581060d03bc94b2c738815a6547f2caca0cd7cd2
Ramon Quitales <ramon@pulumi.com>
2024-05-24 14:28:37 -0400

    tests: add test case to repro #3022 with Jobs and unreachable clusters

--- tests/sdk/java/yamlv2_test.go
+++ tests/sdk/java/yamlv2_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/pulumi/providertest/pulumitest"
+	"github.com/stretchr/testify/assert"
 )
 
 // TestYamlV2 deploys a complex stack using yaml/v2 package.
@@ -20,4 +21,24 @@ func TestYamlV2(t *testing.T) {
 	})
 	test.Preview()
 	test.Up()
+}
+
+// TestJobUnreachable ensures that a panic does not occur when diffing Job resources against an unreachable API server.
+// https://github.com/pulumi/pulumi-kubernetes/issues/3022
+func TestJobUnreachable(t *testing.T) {
+	test := pulumitest.NewPulumiTest(t, "testdata/job-unreachable")
+	t.Logf("into %s", test.Source())
+	t.Cleanup(func() {
+		test.Destroy()
+	})
+	test.Preview()
+
+	// Create the job, but expect it to fail as the job is meant to fail.
+	_, err := test.CurrentStack().Up(test.Context())
+	assert.ErrorContains(t, err, `but the Kubernetes API server reported that it failed to fully initialize or become live`)
+
+	// Re-run the Pulumi program with a malformed kubeconfig to simulate an unreachable API server.
+	// This should not panic annd preview should succeed.
+	test.UpdateSource("testdata/job-unreachable/step2")
+	test.Preview()
 }
