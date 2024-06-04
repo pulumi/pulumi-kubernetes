d96468bd83119ab913f81144de30d41cacd3a52b
Eron Wright <eron@pulumi.com>
2024-06-04 10:56:22 -0700

    integration test

---
+++ tests/sdk/java/chartv4_test.go
@@ -0,0 +1,20 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/pulumi/providertest/pulumitest"
+	"github.com/pulumi/pulumi/sdk/v3/go/auto/optup"
+)
+
+// TestChartV4 deploys a complex stack using chart/v4 package.
+func TestChartv4(t *testing.T) {
+	test := pulumitest.NewPulumiTest(t, "testdata/chartv4")
+	t.Logf("into %s", test.Source())
+	t.Cleanup(func() {
+		test.Destroy()
+	})
+	test.Preview()
+	test.Up()
+	test.Up(optup.ExpectNoChanges())
+}
