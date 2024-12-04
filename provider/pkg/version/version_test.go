a184b31871eca6f90c0581f1849156a69e237d5b
Bryce Lampe <bryce@pulumi.com>
2024-12-04 13:58:35 -0800

    parse makefile directly

--- provider/pkg/version/version_test.go
+++ provider/pkg/version/version_test.go
@@ -3,6 +3,7 @@ package version
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"testing"
 
 	"github.com/blang/semver"
@@ -15,12 +16,17 @@ func TestUserAgent(t *testing.T) {
 }
 
 func TestKubernetesMinorVersion(t *testing.T) {
-	v := os.Getenv("KUBE_VERSION")
-	if v == "" {
-		t.Skip("KUBE_VERSION isn't set")
-	}
 
-	version, err := semver.ParseTolerant(v)
+	re, err := regexp.Compile(`KUBE_VERSION\s+\?=\s+v(?P<version>.*)`)
+	require.NoError(t, err)
+
+	mf, err := os.ReadFile("../../../Makefile")
+	require.NoError(t, err)
+
+	matches := re.FindStringSubmatch(string(mf))
+	require.Len(t, matches, 2)
+
+	version, err := semver.ParseTolerant(matches[1])
 	require.NoError(t, err)
 
 	mod, err := os.ReadFile("../../go.mod")
@@ -28,6 +34,5 @@ func TestKubernetesMinorVersion(t *testing.T) {
 
 	want := fmt.Sprintf("k8s.io/api v0.%d", version.Minor)
 
-	assert.Contains(t, string(mod), want, "KUBE_VERSION=%s doesn't match go.mod's minor version", v)
-
+	assert.Contains(t, string(mod), want, "KUBE_VERSION=v%s doesn't match go.mod's minor version", version)
 }
