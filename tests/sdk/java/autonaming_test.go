22c9b2de3cac650e8be140a4c9af3a68479a214a
Mikhail Shilkov <github@mikhail.io>
2025-01-06 19:07:09 +0000

    Add an e2e test

---
+++ tests/sdk/java/autonaming_test.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/pulumi/providertest/pulumitest"
+	"github.com/pulumi/providertest/pulumitest/opttest"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestAutonaming ensures that custom resource autonaming configuration works as expected.
+func TestAutonaming(t *testing.T) {
+	test := pulumitest.NewPulumiTest(t, "testdata/autonaming", opttest.SkipInstall(), opttest.Env("PULUMI_EXPERIMENTAL", "1"))
+	t.Logf("into %s", test.Source())
+	t.Cleanup(func() {
+		test.Destroy(t)
+	})
+	test.Preview(t)
+	up := test.Up(t)
+	nsname, ok := up.Outputs["nsname"].Value.(string)
+	assert.True(t, ok)
+	assert.Contains(t, nsname, "autonaming-ns-") // project + name + random suffix
+}
